4b366d91fadc0153a2c5f116eb50e28d52ba8dc8
igrekus <upload234@mail.ru>
2019-12-28 17:06:08 +0300

    WIP: implement text extractor

--- fiddle.py
+++ fiddle.py
@@ -1,28 +1,68 @@
-# from coconut.convenience import *
-#
-# inp = ".>>>VV<<V"
-#
-# coconut_eval('"hi" |> print')
-#
-#
+import os
+import tarfile
+import zipfile
 
 
-def run(move):
-    pass
+root_arc = 'find_the_key.tar.bz2'
+current_dir = 'data'
+temp = 'temp'
+
+wrong_file = [
+    'Key in another file',
+    "It's not the file you're looking for",
+    'The key is somewhere but not here',
+]
 
 
-char, *path = '@VVVV>>>>>>>>>>V<<VVVV<<<VV>>>'
-lines = ''.join(path).split('V')
-outp = ''
+def filter_files(l):
+    return [f for f in os.listdir(l) if '.tar' in f or f.endswith('.zip') or f.endswith('.txt')]
 
-spaces = 0
-for line in lines:
-    if '<' in line:
-        spaces -= len(line)
 
-    outp += ' ' * spaces + char * (len(line) + 1) + '\n'
+def extract_tar(file, target_dir):
+    with tarfile.open(file) as tar:
+        tar.extractall(target_dir)
+    tar.close()
 
-    if '>' in line:
-        spaces += len(line)
 
-print(outp)
+def extract_zip(file, target_dir):
+    with zipfile.ZipFile(file) as z:
+        z.extractall(target_dir)
+    z.close()
+
+
+def process_txt(dir_):
+    out_txt = list()
+    for f in os.listdir(dir_):
+        with open(f'{dir_}\\{f}', mode='rt', encoding='utf-8') as txt:
+            out_txt += txt.readlines() + ['\n']
+        if os.path.isfile(f'{dir_}\\{f}'):
+            os.remove(f'{dir_}\\{f}')
+    with open('res.txt', mode='at', encoding='utf-8') as out:
+        out.writelines(['\n<<<\n'] + out_txt + ['\n>>>'])
+
+
+def extract(source_dir, target):
+    files = filter_files(source_dir)
+    new_current = f'{source_dir}\\{target}'
+    for f in files:
+        f_path = f'{source_dir}\\{f}'
+        if f.endswith('.txt'):
+            process_txt(source_dir)
+            return
+        elif '.tar' in f:
+            extract_tar(f_path, new_current)
+            extract(new_current, temp)
+        elif f.endswith('.zip'):
+            extract_zip(f_path, new_current)
+            extract(new_current, temp)
+
+        if os.path.isfile(f_path):
+            if 'Find_the_key.tar.bz2' not in f_path:
+                os.remove(f_path)
+
+    print('rmdir', new_current)
+    os.rmdir(new_current)
+
+
+extract(current_dir, temp)
+
