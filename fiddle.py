42ae777265b22c4a15e0a78b7a00cbe9f0cfad23
igrekus <upload234@mail.ru>
2020-01-27 15:26:51 +0300

    WIP: implement algorithm

--- fiddle.py
+++ fiddle.py
@@ -6,7 +6,6 @@
 # + если одинаковы 2, то вернуть "One Pair", иначе
 # - если одинаковы 2 и 2, то вернуть "Two Pairs", иначе
 # - если одинаковы 3, то вернуть "Three of a Kind", иначе
-
 # - если одинаковы 3 и 2 ([1,1,3,3,3]), то вернуть "Full House", иначе
 # - вернуть "Nothing".
 # сигнатура def check_combination(cards: list) -> str:
@@ -39,36 +38,54 @@ def is_kind_3(seq):
     return bool(count)
 
 
-def has_one_pair(seq):
+def is_one_pair(seq):
     nums = set(seq)
     count = [n for n in nums if seq.count(n) == 2]
     return bool(count) and len(count) == 1
 
 
-def has_two_pairs(seq):
+def is_two_pairs(seq):
     nums = set(seq)
     count = [n for n in nums if seq.count(n) == 2]
     return bool(count) and len(count) == 2
 
 
 def is_fullhouse(seq):
-    return has_set_of_len(seq, 2) and has_set_of_len(seq, 3)
+    return is_kind_3(seq) and is_one_pair(hand)
 
 
 def check_combination(hand):
-    print(hand)
-    print('same', is_impossible(hand))
-    print('straight', is_straight(hand))
-    print('4 kind', is_kind_4(hand))
-    print('one pair', has_one_pair(hand))
-    print('two pairs', has_two_pairs(hand))
-    print('kind 3', is_kind_3(hand))
-    print('fullhouse', is_kind_3(hand) and has_one_pair(hand))
+    # tests =
+    # '1000000'
+    # '0100000'
+    # '0010000'
+    # '0001000'
+    # '0000100'
+    # '0000010'
+    # '0000001'
+    if is_impossible(hand):
+        return 'Impossible'
+    elif is_straight(hand):
+        return 'Straight'
+    elif is_kind_4(hand):
+        return 'Four of a Kind'
+    elif is_fullhouse(hand):
+        return 'Full House'
+    elif is_kind_3(hand):
+        return 'Three of a Kind'
+    elif is_two_pairs(hand):
+        return 'Two Pairs'
+    elif is_one_pair(hand):
+        return 'One Pair'
+    else:
+        return 'Nothing'
 
 
-cards = [i for i in range(14)] * 4
-hand = random.sample(cards, 5)
+cards = [i for i in range(1, 15)] * 4
 
-# check_combination(hand)
-check_combination([2, 4, 4, 2, 2])
-# check_combination([2, 3, 4, 5, 6])
+# hand = [2, 4, 4, 5, 5]
+
+for _ in range(10):
+    hand = random.sample(cards, 5)
+    print(list(sorted(hand)))
+    print(check_combination(hand))
