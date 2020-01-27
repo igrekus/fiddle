0afe7948336ac244c0b85e547c7c80593c670f9f
igrekus <upload234@mail.ru>
2020-01-27 18:33:38 +0300

    WIP: add tests for check_combination
    + dataset

--- tests/test_cards.py
+++ tests/test_cards.py
@@ -1,6 +1,9 @@
+import json
+
 from pyexpect import expect
 
-from fiddle import is_kind_5, is_straight, is_kind_4, is_kind_3, is_kind_2, is_two_pairs, is_full_house
+from fiddle import is_kind_5, is_straight, is_kind_4, is_kind_3, is_kind_2, is_two_pairs, is_full_house, \
+    check_combination
 
 
 def test_is_impossible():
@@ -48,3 +51,22 @@ def test_is_full_house():
     expect(is_full_house([1, 2, 3, 4, 5])).to_equal(False)
     expect(is_full_house([1, 1, 3, 2, 2])).to_equal(False)
     expect(is_full_house([1, 1, 1, 2, 3])).to_equal(False)
+
+
+def test_check_combination():
+    expect(check_combination([1, 2, 3, 4, 5])).to_equal('Straight')
+    expect(check_combination([1, 1, 1, 1, 1])).to_equal('Impossible')
+    expect(check_combination([1, 1, 1, 1, 2])).to_equal('Four of a Kind')
+    expect(check_combination([1, 1, 2, 2, 2])).to_equal('Full House')
+    expect(check_combination([2, 2, 1, 1, 1])).to_equal('Full House')
+    expect(check_combination([2, 3, 1, 1, 1])).to_equal('Three of a Kind')
+    expect(check_combination([2, 2, 3, 1, 1])).to_equal('Two Pair')
+    expect(check_combination([2, 2, 3, 4, 1])).to_equal('Pair')
+    expect(check_combination([2, 5, 3, 7, 1])).to_equal('Nothing')
+
+    with open('D:\\work\\python\\fiddle\\dataset.json', mode='rt', encoding='utf-8') as f:
+        js = json.loads(''.join(f.readlines()))
+
+    for k, v in js.items():
+        ns = [int(s) for s in k.split(' ')]
+        expect(check_combination(ns)).to_equal(v)
