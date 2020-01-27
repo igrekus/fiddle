23c68093395af0c679b5717f668af09156cf7c5d
igrekus <upload234@mail.ru>
2020-01-27 17:25:38 +0300

    FIX: function names in tests

--- tests/test_cards.py
+++ tests/test_cards.py
@@ -1,11 +1,11 @@
 from pyexpect import expect
 
-from fiddle import is_impossible, is_straight, is_kind_4, is_kind_3, is_one_pair, is_two_pairs, is_full_house
+from fiddle import is_kind_5, is_straight, is_kind_4, is_kind_3, is_kind_2, is_two_pairs, is_full_house
 
 
 def test_is_impossible():
-    expect(is_impossible([1, 1, 1, 1, 1])).to_equal(True)
-    expect(is_impossible([2, 1, 1, 1, 1])).to_equal(False)
+    expect(is_kind_5([1, 1, 1, 1, 1])).to_equal(True)
+    expect(is_kind_5([2, 1, 1, 1, 1])).to_equal(False)
 
 
 def test_is_straight():
@@ -27,10 +27,10 @@ def test_is_kind_3():
 
 
 def test_is_one_pair():
-    expect(is_one_pair([1, 2, 3, 4, 4])).to_equal(True)
-    expect(is_one_pair([1, 2, 3, 3, 4])).to_equal(True)
-    expect(is_one_pair([1, 1, 3, 4, 4])).to_equal(False)
-    expect(is_one_pair([1, 5, 3, 4, 2])).to_equal(False)
+    expect(is_kind_2([1, 2, 3, 4, 4])).to_equal(True)
+    expect(is_kind_2([1, 2, 3, 3, 4])).to_equal(True)
+    expect(is_kind_2([1, 1, 3, 4, 4])).to_equal(False)
+    expect(is_kind_2([1, 5, 3, 4, 2])).to_equal(False)
 
 
 def test_is_two_pairs():
