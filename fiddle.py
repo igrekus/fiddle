3fdd2fb0bd9b30a805ba5234c0907f8cce13b798
igrekus <upload234@mail.ru>
2020-01-27 19:21:17 +0300

    REF: more functional tomfoolery

--- fiddle.py
+++ fiddle.py
@@ -1,18 +1,12 @@
-# Итак, новая задача для всех желающих попробовать свои силы, в этот раз попроще (Никита жаловался).
-# Реализовать функцию, которая принимает на вход список из 5 целых положительных чисел и возвращает название комбинации в покере:
-# + если одинаковы 5, то вернуть "Impossible", иначе
-# + если есть 5 последовательных ([1,2,3,4,5]), то вернуть "Straight", иначе
-# + если одинаковы 4, то вернуть "Four of a Kind", иначе
-# + если одинаковы 2, то вернуть "One Pair", иначе
-# - если одинаковы 2 и 2, то вернуть "Two Pairs", иначе
-# - если одинаковы 3, то вернуть "Three of a Kind", иначе
-# - если одинаковы 3 и 2 ([1,1,3,3,3]), то вернуть "Full House", иначе
-# - вернуть "Nothing".
-# сигнатура def check_combination(cards: list) -> str:
-
-
-def is_straight(seq):
-    return len(set(seq)) == 5 and (max(seq) - min(seq)) == 4
+def partial(func, *args, **keywords):
+    def newfunc(*fargs, **fkeywords):
+        newkeywords = keywords.copy()
+        newkeywords.update(fkeywords)
+        return func(*(args + fargs), **newkeywords)
+    newfunc.func = func
+    newfunc.args = args
+    newfunc.keywords = keywords
+    return newfunc
 
 
 def has_seq(seq, l):
@@ -25,24 +19,8 @@ def is_kind(seq, length, times=1):
     return tst and ln == times
 
 
-def is_kind_5(seq):
-    return is_kind(seq, 5)
-
-
-def is_kind_4(seq):
-    return is_kind(seq, 4)
-
-
-def is_kind_3(seq):
-    return is_kind(seq, 3)
-
-
-def is_kind_2(seq):
-    return is_kind(seq, 2)
-
-
-def is_two_pairs(seq):
-    return is_kind(seq, 2, 2)
+def is_straight(seq):
+    return len(set(seq)) == 5 and (max(seq) - min(seq)) == 4
 
 
 labels = {
@@ -51,12 +29,19 @@ labels = {
     8: 'Four of a Kind',
     4: 'Three of a Kind',
     5: 'Full House',
-    2: 'Two Pair',
-    1: 'Pair'
+    2: 'Two Pairs',
+    1: 'One Pair'
 }
 
-test_funcs = [is_straight, is_kind_5, is_kind_4, is_kind_3, is_two_pairs, is_kind_2]
+w_partial = [
+    is_straight,
+    partial(is_kind, length=5),
+    partial(is_kind, length=4),
+    partial(is_kind, length=3),
+    partial(is_kind, length=2, times=2),
+    partial(is_kind, length=2)
+]
 
 
 def check_combination(hand):
-    return labels.get(int(''.join([str(int(f(hand))) for f in test_funcs]), 2), 'Nothing')
+    return labels.get(int(''.join([str(int(f(hand))) for f in w_partial]), 2), 'Nothing')
