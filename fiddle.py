f1a851e143b81fb5fada783bb8a5434765e8d3e8
igrekus <upload234@mail.ru>
2020-01-28 17:13:20 +0300

    REF: factor out the last unneeded function

--- fiddle.py
+++ fiddle.py
@@ -9,35 +9,25 @@ def partial(func, *args, **keywords):
     return newfunc
 
 
-def has_seq(seq, l):
-    count = [n for n in set(seq) if seq.count(n) == l]
-    return bool(count), len(count)
-
-
-def is_kind(seq, length, times=1):
-    tst, ln = has_seq(seq, length)
-    return tst and ln == times
-
-
-labels = {
-    32: 'Straight',
-    16: 'Impossible',
-    8: 'Four of a Kind',
-    4: 'Three of a Kind',
-    5: 'Full House',
-    2: 'Two Pairs',
-    1: 'One Pair'
-}
-
-test_funcs = [
-    lambda seq: partial(is_kind, length=1, times=5)(seq) and (max(seq) - min(seq)) == 4,
-    partial(is_kind, length=5),
-    partial(is_kind, length=4),
-    partial(is_kind, length=3),
-    partial(is_kind, length=2, times=2),
-    partial(is_kind, length=2)
-]
+def has_seq_of(seq, length, times=1):
+    count = [n for n in set(seq) if seq.count(n) == length]
+    return bool(count) and len(count) == times
 
 
 def check_combination(hand):
-    return labels.get(int(''.join([str(int(f(hand))) for f in test_funcs]), 2), 'Nothing')
+    return {
+        32: 'Straight',
+        16: 'Impossible',
+        8: 'Four of a Kind',
+        4: 'Three of a Kind',
+        5: 'Full House',
+        2: 'Two Pairs',
+        1: 'One Pair'
+    }.get(int(''.join([str(int(f(hand))) for f in [
+        lambda seq: partial(has_seq_of, length=1, times=5)(seq) and (max(seq) - min(seq)) == 4,
+        partial(has_seq_of, length=5),
+        partial(has_seq_of, length=4),
+        partial(has_seq_of, length=3),
+        partial(has_seq_of, length=2, times=2),
+        partial(has_seq_of, length=2)
+    ]]), 2), 'Nothing')
