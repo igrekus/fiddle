2cebde114a075ec308e2bb21ee7a8df8f8b5cf73
igrekus <upload234@mail.ru>
2020-02-03 13:04:59 +0300

    REF: push ifs into functions

--- fiddle.py
+++ fiddle.py
@@ -11,13 +11,15 @@ from random import choice, choices
 from string import ascii_lowercase, ascii_uppercase, digits
 
 
+def add_upper(st, should=False):
+    return choice(ascii_uppercase) + st[1:] if should else st
+
+
+def add_digit(st, should=False):
+    return st[:-1] + choice(digits) if should else st
+
+
 def password(length: int, use_upper=False, use_digits=False) -> str:
     if length < 8:
         raise ValueError('Minimal password length is 8')
-
-    res = ''.join(choices(ascii_lowercase, k=length))
-    if use_upper:
-        res = choice(ascii_uppercase) + res[1:]
-    if use_digits:
-        res = res[:-1] + choice(digits)
-    return res
+    return add_digit(add_upper(''.join(choices(ascii_lowercase, k=length)), should=use_upper), should=use_digits)
