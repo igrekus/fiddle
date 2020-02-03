8178e955524102aa7e0c14cc4896d3904d32633b
igrekus <upload234@mail.ru>
2020-02-03 23:02:05 +0300

    REF: use one function

--- fiddle.py
+++ fiddle.py
@@ -2,29 +2,39 @@ from functools import update_wrapper
 from random import choice, choices
 from string import ascii_lowercase, ascii_uppercase, digits
 
+
 class chain:
     def __init__(self, function):
         self.function = function
         update_wrapper(self, function)
+
     def __ror__(self, other):
         return self.function(other)
+
     def __call__(self, *args, **kwargs):
         return chain(lambda x: self.function(x, *args, **kwargs))
 
-@chain
-def add_upper(it, should=False):
-    for el in it: yield choice(ascii_uppercase) + el[1:] if should else el
+
+def add(st, what='', at=0, should=False):
+    return st[:at] + choice(what) + st[at + 1:] if should else st
+
 
 @chain
-def add_digit(it, should=False):
-    for el in it: yield el[:-1] + choice(digits) if should else el
+def with_chars(container, what, at=0, should=False):
+    yield from (add(el, what, at, should) for el in container)
+
 
 @chain
 def base(it, k=8):
-    for el in it: yield ''.join(choices(el, k=k))
+    yield from (''.join(choices(el, k=k)) for el in it)
 
 
 def password(length: int, use_upper=False, use_digits=False) -> str:
     if length < 8:
         raise ValueError('Minimal password length is 8')
-    return list([ascii_lowercase] | base(k=length) | add_upper(should=use_upper) | add_digit(should=use_digits))[0]
+    return list(
+        [ascii_lowercase]
+        | base(k=length)
+        | with_chars(ascii_uppercase, at=0, should=use_upper)
+        | with_chars(digits, at=1, should=use_digits)
+    )[0]
