f89cc1179f2cbe1be64fb4cdb3f23b9624c75d16
igrekus <upload234@mail.ru>
2020-02-03 13:46:46 +0300

    FEAT: class-based solution

--- fiddle.py
+++ fiddle.py
@@ -11,19 +11,23 @@ from random import choice, choices
 from string import ascii_lowercase, ascii_uppercase, digits
 
 
-def add_upper(st, should=False):
-    return choice(ascii_uppercase) + st[1:] if should else st
+class Password:
+    def __init__(self, length=8):
+        self.password = ''.join(choices(ascii_lowercase, k=length))
 
+    def upper(self, should=False):
+        self._add_to(what=ascii_uppercase, where=0, should=should)
+        return self
 
-def add_digit(st, should=False):
-    return st[:-1] + choice(digits) if should else st
+    def digit(self, should=False):
+        self._add_to(what=digits, where=1, should=should)
+        return self
 
-
-def base(st, k=8):
-    return ''.join(choices(st, k=k))
+    def _add_to(self, what, where=0, should=False):
+        self.password = self.password[:where] + choice(what) + self.password[where + 1:] if should else self.password
 
 
 def password(length: int, use_upper=False, use_digits=False) -> str:
     if length < 8:
         raise ValueError('Minimal password length is 8')
-    return add_digit(add_upper(base(ascii_lowercase, k=length), should=use_upper), should=use_digits)
+    return Password(length=length).upper(use_upper).digit(use_digits).password
