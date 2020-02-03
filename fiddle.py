fd6a35c3a334a1c2d679e5849c3258e8595736fd
igrekus <upload234@mail.ru>
2020-02-03 22:44:19 +0300

    WIP: implement functional call chain

--- fiddle.py
+++ fiddle.py
@@ -1,31 +1,30 @@
-# Новая задача: написать функцию генерации пароля, сигнатура(не менять!)
-# def password(length: int, use_upper=False, use_digits=False) -> str:
-# + Функция возвращает случайную последовательность длины length, состоящую только из латиницы и цифр (опционально)
-# + Если не заданы флаги то функция возвращает пароль указанной длины из различных строчных(маленьких) букв
-# + Если заданная длина пароля меньше 8 должно быть брошено исключение ValueError с текстом Minimal password length is 8
-# + Если флаг use_upper = True то последовательность должна содержать минимум 1 Заглавную букву
-# + Если флаг use_digit = True то последовательность должна содержать минимум 1 цифру (0-9)
-# + Не при каких условиях пароль НЕ может состоять только из больших букв или только из цифр.
-
+from functools import update_wrapper
 from random import choice, choices
 from string import ascii_lowercase, ascii_uppercase, digits
 
+class chain:
+    def __init__(self, function):
+        self.function = function
+        update_wrapper(self, function)
+    def __ror__(self, other):
+        return self.function(other)
+    def __call__(self, *args, **kwargs):
+        return chain(lambda x: self.function(x, *args, **kwargs))
 
-class Password:
-    def __init__(self, length=8):
-        self.password = ''.join(choices(ascii_lowercase, k=length))
-        self.where = 0
+@chain
+def add_upper(it, should=False):
+    for el in it: yield choice(ascii_uppercase) + el[1:] if should else el
 
-    def using(self, what='', when=False):
-        self.password = self.password[:self.where] + choice(what) + self.password[self.where + 1:] if when else self.password
-        self.where += 1
-        return self
+@chain
+def add_digit(it, should=False):
+    for el in it: yield el[:-1] + choice(digits) if should else el
+
+@chain
+def base(it, k=8):
+    for el in it: yield ''.join(choices(el, k=k))
 
 
 def password(length: int, use_upper=False, use_digits=False) -> str:
     if length < 8:
         raise ValueError('Minimal password length is 8')
-    return Password(length)\
-        .using(what=ascii_uppercase, when=use_upper)\
-        .using(what=digits, when=use_digits)\
-        .password
+    return list([ascii_lowercase] | base(k=length) | add_upper(should=use_upper) | add_digit(should=use_digits))[0]
