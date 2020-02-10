f08ab8235c01427a2a67f0ac20b33b051622b190
igrekus <upload234@mail.ru>
2020-02-10 17:28:47 +0300

    REF: remove unnesessary dict

--- fiddle.py
+++ fiddle.py
@@ -17,26 +17,16 @@ def repeat_nums(nums, times):
     yield from chain(*repeat(nums, times))
 
 
-def decimal_pairs(places, prev_nums):
-    yield from zip(repeat_place(places, len(prev_nums)), repeat_nums(prev_nums, len(places)))
-
-
-def gen_nums(high_place, prev_nums):
-    return [f'{high} {low}' for high, low in decimal_pairs(high_place, prev_nums)]
-
-
-_10s = ['двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто']
-_100s = ['сто', 'двести', 'триста', 'четыреста', 'пятьсот', 'шестьсот', 'семьсот', 'восемьсот', 'девятьсот']
 ones = ['ноль', 'один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять']
 teens = ['десять', 'одиннадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать']
+_10s = ['двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто']
+_100s = ['сто', 'двести', 'триста', 'четыреста', 'пятьсот', 'шестьсот', 'семьсот', 'восемьсот', 'девятьсот']
 
 first_hundred = ones + teens + gen_nums(_10s, ones)
-first_thousand = first_hundred + gen_nums(_100s, first_hundred)
-
-d = {i: s.replace(' ноль', '') for i, s in zip(range(1000), first_thousand)}
+first_thousand = [s.replace(' ноль', '') for s in first_hundred + gen_nums(_100s, first_hundred)]
 
 
 def to_russian_string(value: int) -> str:
     if value not in range(1000):
         raise ValueError('Input value must be within [0..999]')
-    return d[value]
+    return first_thousand[value]
