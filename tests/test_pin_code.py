2b9fd1a099b8fd7bdce6e2b28540f729af98f214
igrekus <upload234@mail.ru>
2020-02-17 13:32:08 +0300

    REF: rename example string test

--- tests/test_pin_code.py
+++ tests/test_pin_code.py
@@ -2,6 +2,6 @@ from pyexpect import expect
 from fiddle import text_to_pin_code
 
 
-def test_pin_code():
+def test_pin_code_example():
     test = 'example test for testing {"fieldOne":1, "fields":2, "fielD":[1,"22", "e3r4"], "fieldN":"2 3 4"} more unnecessary data'
     expect(text_to_pin_code(test)).to_equal('1210')
