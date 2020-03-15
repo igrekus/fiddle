eed4494cedbd2a55354328662c753e59170315a0
igrekus <upload234@mail.ru>
2020-03-15 12:30:12 +0300

    WIP: update one-liner with groupby solution

--- text-to-pin-code-full-retard.py
+++ text-to-pin-code-full-retard.py
@@ -1,24 +1,13 @@
 # -*- coding: utf-8 -*-
 import re
 import json
-from collections import defaultdict
 from functools import singledispatch
-from itertools import chain
+from itertools import chain, groupby
 from string import ascii_uppercase as uppercase
 
 
 def text_to_pin_code(text):
-    d = defaultdict(set)
-    for el in (
-            int(x) for x in (
-            s for s in (
-            chain(*(_normalize_value(v) for v in (
-                    value for field, value in
-                    json.loads(re.compile(r'^.*(?P<json>{.*}).*$', re.VERBOSE).match(text)['json']).items()
-                    if set(field).intersection(uppercase)))))
-            if s)):
-        d[len(f'{el}')].add(el)
-    return ''.join(f"{len(d.get(n_digit_num, set()))}" for n_digit_num in range(1, 5))
+    return ''.join(f"{len({n: set(vals) for n, vals in groupby(sorted((int(x) for x in (s for s in (chain(*(_normalize_value(v) for v in (value for field, value in json.loads(re.compile(r'^.*(?P<json>{.*}).*$', re.VERBOSE).match(text)['json']).items()if set(field).intersection(uppercase))))) if s))), key=lambda x: len(f'{x}'))}.get(n_digit_num, set()))}" for n_digit_num in range(1, 5))
 
 
 @singledispatch
