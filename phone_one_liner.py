ac060cdf06233a4916a755872fde76a9bc991602
igrekus <upload234@mail.ru>
2020-04-01 23:51:27 +0300

    WIP: add one-liner phone number solution

--- phone_one_liner.py
+++ phone_one_liner.py
@@ -7,14 +7,3 @@ def format_numbers(phone_number: str) -> str:
             '  ' + ''.join([c for c in phone_number if c.isdigit()])[1:]
         )
     ])
-
-
-phones = [
-    '+7(909)111-22-33',
-    '8(909)111-22-33',
-    '8(909)111 22 33',
-    '8(909)111 22-33',
-]
-
-for ph in phones:
-    print('out:', format_numbers(ph))
