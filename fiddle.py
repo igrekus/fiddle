b86d5e69f85c5521c32afc69df48f4cf9bc31169
igrekus <upload234@mail.ru>
2020-04-01 22:06:22 +0300

    WIP: functional solution stub

--- fiddle.py
+++ fiddle.py
@@ -1,15 +1,27 @@
+class rpartial:
+    def __new__(cls, func, *args, **keywords):
+        self = super().__new__(cls)
+        self.func = func
+        self.args = args
+        self.keywords = keywords
+        return self
+    def __call__(self, *args, **kwargs):
+        kw = self.keywords.copy()
+        kw.update(kwargs)
+        return self.func(*(args + self.args), **kw)
+
+
+old = list('()- ') + ['+7']
+new = [''] * 4 + ['8']
+
+
 def format_numbers(phone_number: str) -> str:
-    pn = phone_number \
-        .replace('(', '') \
-        .replace(')', '') \
-        .replace('-', '') \
-        .replace(' ', '')\
-        .replace('+7', '8')
-    print(pn)
-    code = pn[1:4]
-    three = pn[4:7]
-    two_1 = pn[7:9]
-    two_2 = pn[9:12]
+    for f in [rpartial(str.replace, o, n) for o, n in zip(old, new)]:
+        phone_number = f(phone_number)
+    code = phone_number[1:4]
+    three = phone_number[4:7]
+    two_1 = phone_number[7:9]
+    two_2 = phone_number[9:12]
     return f'+7({code}){three}-{two_1}-{two_2}'
 
 
@@ -21,4 +33,4 @@ phones = [
 ]
 
 for ph in phones:
-    print(format_numbers(ph))
+    print('out:', format_numbers(ph))
