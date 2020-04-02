179d603c1b183e3b0445cbb29f6ed7626ffe5c58
igrekus <upload234@mail.ru>
2020-04-03 01:52:42 +0300

    FEAT: add classic solution to longest common sequence

--- max_string_func.py
+++ max_string_func.py
@@ -5,10 +5,3 @@ def max_common_string(first_string: str, second_string: str) -> str:
     return \
         f + max_common_string(fs, ss) if f == s else \
         max(max_common_string(first_string, ss), max_common_string(fs, second_string), key=len)
-
-
-print(max_common_string('aaaa', 'aa'))
-print(max_common_string('abc', 'acb'))
-print(max_common_string('abcd', 'abdc'))
-print(max_common_string('thisisatest', 'testing123testing'))   # tsitest
-print(max_common_string('axxxxxxxxxb', 'yyyyayyyyyb'))   # ab
