f31d970bd85a33cee4df8338a574304fcdb201d8
igrekus <upload234@mail.ru>
2020-04-19 20:40:24 +0300

    REF: brain farts clean uo

--- fiddle.py
+++ fiddle.py
@@ -62,10 +62,10 @@ def counts_db(params):
     return sum(v == params[0] for v in db.values()) or '0'
 
 def get_wal(params):
+    set_com = f'SET {params[0]} '
+    unset_com = f'UNSET {params[0]}'
     for log in reversed(wal):
         for com in reversed(log):
-            set_com = f'SET {params[0]} '
-            unset_com = f'UNSET {params[0]}'
             if com == unset_com:
                 return 'MULL'
             if com.startswith(set_com):
@@ -125,7 +125,7 @@ def execute(com_str, silent=False):
     op = op.upper()
     if op == 'END':
         return False
-    if res := commands.get(op.upper(), default)(params):
+    if res := commands.get(op, default)(params):
         if not silent:
             print(res)
     return True
