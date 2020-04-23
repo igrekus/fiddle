8fcfbcfd90b1314771b9560171e084f571840bec
igrekus <upload234@mail.ru>
2020-04-23 21:17:34 +0300

    REF: add container abstraction

--- fiddle.py
+++ fiddle.py
@@ -1,21 +1,29 @@
 class Bottles:
     def verse(self, num):
         if num == 0:
-            return '''Нет бутылок пива на стене, нет бутылок пива!
-Сходи в магазин, купи ещё, 99 бутылок пива на стене
+            return f'''Нет {self.container(num)} пива на стене, нет {self.container(num)} пива!
+Сходи в магазин, купи ещё, 99 {self.container(num - 1)} пива на стене
 '''
         elif num == 1:
-            return '''1 бутылка пива на стене, 1 бутылка пива!
-Возьми одну, передай мне, нет бутылок пива на стене
+            return f'''1 {self.container(num)} пива на стене, {num} {self.container(num)} пива!
+Возьми одну, передай мне, нет {self.container(num - 1)} пива на стене
 '''
         elif num == 2:
-            return f'''{num} бутылки пива на стене, {num} бутылки пива!
-Возьми одну, передай мне, {num - 1} бутылка пива на стене
+            return f'''{num} {self.container(num)} пива на стене, {num} {self.container(num)} пива!
+Возьми одну, передай мне, {num - 1} {self.container(num - 1)} пива на стене
 '''
         else:
-            return f'''{num} бутылок пива на стене, {num} бутылок пива!
-Возьми одну, передай мне, {num - 1} бутылок пива на стене
+            return f'''{num} {self.container(num)} пива на стене, {num} {self.container(num)} пива!
+Возьми одну, передай мне, {num - 1} {self.container(num - 1)} пива на стене
 '''
+
+    def container(self, num):
+        if num in (1, 21, 31, 41, 51, 61, 71, 81, 91):
+            return 'бутылка'
+        elif num in (2, 3, 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54, 62, 63, 64, 72, 73, 74, 82, 83, 84, 92, 93, 94):
+            return 'бутылки'
+        else:
+            return 'бутылок'
 
     def verses(self, upper, lower):
         return '\n'.join(filter(bool, [self.verse(num) for num in reversed(range(lower, upper + 1))]))
