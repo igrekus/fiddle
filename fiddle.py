2a32fc713c5e2d77b04bc638c2e33ea0e3f8f0e5
igrekus <upload234@mail.ru>
2020-05-07 00:15:16 +0300

    REF: replace random order song class with an orderer class

--- fiddle.py
+++ fiddle.py
@@ -2,8 +2,20 @@ import itertools
 import random
 
 
+class DefaultOrder:
+    def order(self, data):
+        return data
+
+
+class RandomOrder:
+    def order(self, data):
+        d = list(data)
+        random.shuffle(d)
+        return d
+
+
 class Song:
-    def __init__(self):
+    def __init__(self, orderer=None):
         self.data = [
             'the horse and the hound and the horn,\nThat belong to ',
             'the farmer sowing his corn,\nThat kept ',
@@ -18,6 +30,8 @@ class Song:
             'the malt\nThat lay in ',
             'the house that Jack built '
         ]
+        self._orderer = DefaultOrder() if orderer is None else orderer
+        self.data = self._orderer.order(self.data)
 
     def recite(self):
         return '\n'.join(self.line(i) for i in range(1, len(self.data) + 1))
@@ -32,31 +46,30 @@ class Song:
         return ''.join(self._parts(num))
 
 
-class RandomSong(Song):
+class EchoSong(Song):
     def __init__(self):
         super().__init__()
-        random.shuffle(self.data)
 
-
-class EchoSong(Song):
     def _parts(self, num):
         return itertools.chain(*[[l, r] for l, r in zip(super()._parts(num), super()._parts(num))])
 
 
-class EchoRandomSong(RandomSong):
+class EchoRandomSong(Song):
+    def __init__(self):
+        super().__init__(orderer=RandomOrder())
     def _parts(self, num):
         return itertools.chain(*[[l, r] for l, r in zip(super()._parts(num), super()._parts(num))])
 
 
 def song(rnd=False, echo=False):
     if rnd and not echo:
-        return RandomSong().recite()
+        return Song(orderer=RandomOrder()).recite()
     elif not rnd and echo:
         return EchoSong().recite()
     elif rnd and echo:
         return EchoRandomSong().recite()
     else:
-        return Song().recite() if not rnd else RandomSong().recite()
+        return Song().recite()
 
 
 def line(num):
