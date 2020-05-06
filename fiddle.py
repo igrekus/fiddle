783746468d187cf06b2f71a63514ee258181669b
igrekus <upload234@mail.ru>
2020-05-06 21:21:46 +0300

    REF: make random song not affect superclass

--- fiddle.py
+++ fiddle.py
@@ -3,20 +3,21 @@ import random
 
 
 class Song:
-    data = [
-        'the horse and the hound and the horn,\nThat belong to ',
-        'the farmer sowing his corn,\nThat kept ',
-        'the rooster that crow\'d in the morn,\nThat waked ',
-        'the priest all shaven and shorn,\nThat married ',
-        'the man all tattered and torn,\nThat kissed ',
-        'the maiden all forlorn,\nThat milked ',
-        'the cow with the crumpled horn,\nThat tossed ',
-        'the dog,\nThat worried ',
-        'the cat,\nThat killed ',
-        'the rat,\nThat ate ',
-        'the malt\nThat lay in ',
-        'the house that Jack built '
-    ]
+    def __init__(self):
+        self.data = [
+            'the horse and the hound and the horn,\nThat belong to ',
+            'the farmer sowing his corn,\nThat kept ',
+            'the rooster that crow\'d in the morn,\nThat waked ',
+            'the priest all shaven and shorn,\nThat married ',
+            'the man all tattered and torn,\nThat kissed ',
+            'the maiden all forlorn,\nThat milked ',
+            'the cow with the crumpled horn,\nThat tossed ',
+            'the dog,\nThat worried ',
+            'the cat,\nThat killed ',
+            'the rat,\nThat ate ',
+            'the malt\nThat lay in ',
+            'the house that Jack built '
+        ]
 
     def recite(self):
         return '\n'.join(self.line(i) for i in range(1, len(self.data) + 1))
@@ -33,6 +34,7 @@ class Song:
 
 class RandomSong(Song):
     def __init__(self):
+        super().__init__()
         random.shuffle(self.data)
 
 
@@ -54,4 +56,4 @@ def line(num):
     return Song().line(num)
 
 
-print(song(rnd=True))
+print(song(echo=True))
