2d9a66ff7c1ec29726ec2fca049245cac81428ce
igrekus <upload234@mail.ru>
2020-05-06 23:49:08 +0300

    FIX: properly use mocked random as expected value

--- tests/test_echo_random_song.py
+++ tests/test_echo_random_song.py
@@ -21,178 +21,175 @@ def shuffle(it):
         it[i] = new[i]
 
 
-def test_random_song():
-    expected = dedent("""This is the priest all shaven and shorn,
-    That married the priest all shaven and shorn,
-    That married.
-
-    This is the cow with the crumpled horn,
-    That tossed the cow with the crumpled horn,
-    That tossed the priest all shaven and shorn,
-    That married the priest all shaven and shorn,
-    That married.
-
+def test_echo_random_song():
+    expected = dedent("""    This is the farmer sowing his corn,
+    That kept the farmer sowing his corn,
+    That kept.
+    
     This is the maiden all forlorn,
     That milked the maiden all forlorn,
-    That milked the cow with the crumpled horn,
-    That tossed the cow with the crumpled horn,
-    That tossed the priest all shaven and shorn,
-    That married the priest all shaven and shorn,
-    That married.
-
+    That milked the farmer sowing his corn,
+    That kept the farmer sowing his corn,
+    That kept.
+    
     This is the malt
     That lay in the malt
     That lay in the maiden all forlorn,
     That milked the maiden all forlorn,
-    That milked the cow with the crumpled horn,
-    That tossed the cow with the crumpled horn,
-    That tossed the priest all shaven and shorn,
-    That married the priest all shaven and shorn,
-    That married.
-
-    This is the rooster that crow'd in the morn,
-    That waked the rooster that crow'd in the morn,
-    That waked the malt
-    That lay in the malt
-    That lay in the maiden all forlorn,
-    That milked the maiden all forlorn,
-    That milked the cow with the crumpled horn,
-    That tossed the cow with the crumpled horn,
-    That tossed the priest all shaven and shorn,
-    That married the priest all shaven and shorn,
-    That married.
-
+    That milked the farmer sowing his corn,
+    That kept the farmer sowing his corn,
+    That kept.
+    
     This is the dog,
     That worried the dog,
-    That worried the rooster that crow'd in the morn,
-    That waked the rooster that crow'd in the morn,
-    That waked the malt
+    That worried the malt
     That lay in the malt
     That lay in the maiden all forlorn,
     That milked the maiden all forlorn,
-    That milked the cow with the crumpled horn,
-    That tossed the cow with the crumpled horn,
-    That tossed the priest all shaven and shorn,
-    That married the priest all shaven and shorn,
-    That married.
-
-    This is the house that Jack built the house that Jack built the dog,
-    That worried the dog,
-    That worried the rooster that crow'd in the morn,
-    That waked the rooster that crow'd in the morn,
-    That waked the malt
-    That lay in the malt
-    That lay in the maiden all forlorn,
-    That milked the maiden all forlorn,
-    That milked the cow with the crumpled horn,
-    That tossed the cow with the crumpled horn,
-    That tossed the priest all shaven and shorn,
-    That married the priest all shaven and shorn,
-    That married.
-
-    This is the cat,
-    That killed the cat,
-    That killed the house that Jack built the house that Jack built the dog,
-    That worried the dog,
-    That worried the rooster that crow'd in the morn,
-    That waked the rooster that crow'd in the morn,
-    That waked the malt
-    That lay in the malt
-    That lay in the maiden all forlorn,
-    That milked the maiden all forlorn,
-    That milked the cow with the crumpled horn,
-    That tossed the cow with the crumpled horn,
-    That tossed the priest all shaven and shorn,
-    That married the priest all shaven and shorn,
-    That married.
-
-    This is the rat,
-    That ate the rat,
-    That ate the cat,
-    That killed the cat,
-    That killed the house that Jack built the house that Jack built the dog,
-    That worried the dog,
-    That worried the rooster that crow'd in the morn,
-    That waked the rooster that crow'd in the morn,
-    That waked the malt
-    That lay in the malt
-    That lay in the maiden all forlorn,
-    That milked the maiden all forlorn,
-    That milked the cow with the crumpled horn,
-    That tossed the cow with the crumpled horn,
-    That tossed the priest all shaven and shorn,
-    That married the priest all shaven and shorn,
-    That married.
-
+    That milked the farmer sowing his corn,
+    That kept the farmer sowing his corn,
+    That kept.
+    
     This is the horse and the hound and the horn,
     That belong to the horse and the hound and the horn,
-    That belong to the rat,
-    That ate the rat,
-    That ate the cat,
-    That killed the cat,
-    That killed the house that Jack built the house that Jack built the dog,
+    That belong to the dog,
     That worried the dog,
-    That worried the rooster that crow'd in the morn,
-    That waked the rooster that crow'd in the morn,
-    That waked the malt
+    That worried the malt
     That lay in the malt
     That lay in the maiden all forlorn,
     That milked the maiden all forlorn,
-    That milked the cow with the crumpled horn,
-    That tossed the cow with the crumpled horn,
-    That tossed the priest all shaven and shorn,
-    That married the priest all shaven and shorn,
-    That married.
-
-    This is the farmer sowing his corn,
+    That milked the farmer sowing his corn,
     That kept the farmer sowing his corn,
-    That kept the horse and the hound and the horn,
-    That belong to the horse and the hound and the horn,
-    That belong to the rat,
-    That ate the rat,
-    That ate the cat,
-    That killed the cat,
-    That killed the house that Jack built the house that Jack built the dog,
-    That worried the dog,
-    That worried the rooster that crow'd in the morn,
-    That waked the rooster that crow'd in the morn,
-    That waked the malt
-    That lay in the malt
-    That lay in the maiden all forlorn,
-    That milked the maiden all forlorn,
-    That milked the cow with the crumpled horn,
-    That tossed the cow with the crumpled horn,
-    That tossed the priest all shaven and shorn,
-    That married the priest all shaven and shorn,
-    That married.
-
+    That kept.
+    
     This is the man all tattered and torn,
     That kissed the man all tattered and torn,
-    That kissed the farmer sowing his corn,
-    That kept the farmer sowing his corn,
-    That kept the horse and the hound and the horn,
+    That kissed the horse and the hound and the horn,
     That belong to the horse and the hound and the horn,
-    That belong to the rat,
-    That ate the rat,
-    That ate the cat,
-    That killed the cat,
-    That killed the house that Jack built the house that Jack built the dog,
+    That belong to the dog,
     That worried the dog,
-    That worried the rooster that crow'd in the morn,
-    That waked the rooster that crow'd in the morn,
-    That waked the malt
+    That worried the malt
     That lay in the malt
     That lay in the maiden all forlorn,
     That milked the maiden all forlorn,
-    That milked the cow with the crumpled horn,
-    That tossed the cow with the crumpled horn,
-    That tossed the priest all shaven and shorn,
+    That milked the farmer sowing his corn,
+    That kept the farmer sowing his corn,
+    That kept.
+    
+    This is the house that Jack built the house that Jack built the man all tattered and torn,
+    That kissed the man all tattered and torn,
+    That kissed the horse and the hound and the horn,
+    That belong to the horse and the hound and the horn,
+    That belong to the dog,
+    That worried the dog,
+    That worried the malt
+    That lay in the malt
+    That lay in the maiden all forlorn,
+    That milked the maiden all forlorn,
+    That milked the farmer sowing his corn,
+    That kept the farmer sowing his corn,
+    That kept.
+    
+    This is the rooster that crow'd in the morn,
+    That waked the rooster that crow'd in the morn,
+    That waked the house that Jack built the house that Jack built the man all tattered and torn,
+    That kissed the man all tattered and torn,
+    That kissed the horse and the hound and the horn,
+    That belong to the horse and the hound and the horn,
+    That belong to the dog,
+    That worried the dog,
+    That worried the malt
+    That lay in the malt
+    That lay in the maiden all forlorn,
+    That milked the maiden all forlorn,
+    That milked the farmer sowing his corn,
+    That kept the farmer sowing his corn,
+    That kept.
+    
+    This is the priest all shaven and shorn,
     That married the priest all shaven and shorn,
-    That married.\n""")
+    That married the rooster that crow'd in the morn,
+    That waked the rooster that crow'd in the morn,
+    That waked the house that Jack built the house that Jack built the man all tattered and torn,
+    That kissed the man all tattered and torn,
+    That kissed the horse and the hound and the horn,
+    That belong to the horse and the hound and the horn,
+    That belong to the dog,
+    That worried the dog,
+    That worried the malt
+    That lay in the malt
+    That lay in the maiden all forlorn,
+    That milked the maiden all forlorn,
+    That milked the farmer sowing his corn,
+    That kept the farmer sowing his corn,
+    That kept.
+    
+    This is the rat,
+    That ate the rat,
+    That ate the priest all shaven and shorn,
+    That married the priest all shaven and shorn,
+    That married the rooster that crow'd in the morn,
+    That waked the rooster that crow'd in the morn,
+    That waked the house that Jack built the house that Jack built the man all tattered and torn,
+    That kissed the man all tattered and torn,
+    That kissed the horse and the hound and the horn,
+    That belong to the horse and the hound and the horn,
+    That belong to the dog,
+    That worried the dog,
+    That worried the malt
+    That lay in the malt
+    That lay in the maiden all forlorn,
+    That milked the maiden all forlorn,
+    That milked the farmer sowing his corn,
+    That kept the farmer sowing his corn,
+    That kept.
+    
+    This is the cow with the crumpled horn,
+    That tossed the cow with the crumpled horn,
+    That tossed the rat,
+    That ate the rat,
+    That ate the priest all shaven and shorn,
+    That married the priest all shaven and shorn,
+    That married the rooster that crow'd in the morn,
+    That waked the rooster that crow'd in the morn,
+    That waked the house that Jack built the house that Jack built the man all tattered and torn,
+    That kissed the man all tattered and torn,
+    That kissed the horse and the hound and the horn,
+    That belong to the horse and the hound and the horn,
+    That belong to the dog,
+    That worried the dog,
+    That worried the malt
+    That lay in the malt
+    That lay in the maiden all forlorn,
+    That milked the maiden all forlorn,
+    That milked the farmer sowing his corn,
+    That kept the farmer sowing his corn,
+    That kept.
+    
+    This is the cat,
+    That killed the cat,
+    That killed the cow with the crumpled horn,
+    That tossed the cow with the crumpled horn,
+    That tossed the rat,
+    That ate the rat,
+    That ate the priest all shaven and shorn,
+    That married the priest all shaven and shorn,
+    That married the rooster that crow'd in the morn,
+    That waked the rooster that crow'd in the morn,
+    That waked the house that Jack built the house that Jack built the man all tattered and torn,
+    That kissed the man all tattered and torn,
+    That kissed the horse and the hound and the horn,
+    That belong to the horse and the hound and the horn,
+    That belong to the dog,
+    That worried the dog,
+    That worried the malt
+    That lay in the malt
+    That lay in the maiden all forlorn,
+    That milked the maiden all forlorn,
+    That milked the farmer sowing his corn,
+    That kept the farmer sowing his corn,
+    That kept.\n""")
     sf = random.shuffle
     random.shuffle = shuffle
-    res = song(rnd=True, echo=True)
-    expect(res).to_equal(expected)
+    expect(song(rnd=True, echo=True)).to_equal(expected)
     random.shuffle = sf
-
-    print(res)
