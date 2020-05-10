dac3bb5b9a22c3f39f4dc996fd08d08ce85f1a33
igrekus <upload234@mail.ru>
2020-05-11 02:28:12 +0300

    FEAT: add jack song oop solution as a separate file

---
+++ jack_song_opp.py
@@ -0,0 +1,67 @@
+import itertools
+import random
+
+
+class DefaultOrder:
+    def order(self, data):
+        return data
+
+
+class RandomOrder:
+    def order(self, data):
+        d = list(data)
+        random.shuffle(d)
+        return d
+
+
+class DefaultFormat:
+    def format(self, parts):
+        return parts
+
+
+class DoubleFormat:
+    def format(self, parts):
+        return itertools.chain(*[[l, r] for l, r in zip(parts, parts)])
+
+
+class Song:
+    orderers = {True: RandomOrder, False: DefaultOrder}
+    formatters = {True: DoubleFormat, False: DefaultFormat}
+
+    def __init__(self, orderer, formatter):
+        self.data = [
+            'the horse and the hound and the horn,\nThat belong to ',
+            'the farmer sowing his corn,\nThat kept ',
+            'the rooster that crow\'d in the morn,\nThat waked ',
+            'the priest all shaven and shorn,\nThat married ',
+            'the man all tattered and torn,\nThat kissed ',
+            'the maiden all forlorn,\nThat milked ',
+            'the cow with the crumpled horn,\nThat tossed ',
+            'the dog,\nThat worried ',
+            'the cat,\nThat killed ',
+            'the rat,\nThat ate ',
+            'the malt\nThat lay in ',
+            'the house that Jack built '
+        ]
+        self._orderer = orderer
+        self._formatter = formatter
+        self.data = self._orderer.order(self.data)
+
+    def __str__(self):
+        return '\n\n'.join(self._line(i) for i in range(1, len(self) + 1))
+
+    def _line(self, num):
+        return f'This is {self._phrase(num).strip()}.'
+
+    def _parts(self, num):
+        return self._formatter.format(self.data[-num:])
+
+    def _phrase(self, num):
+        return ''.join(self._parts(num))
+
+    def __len__(self):
+        return self.data.__len__()
+
+
+def song(rnd=False, double=False):
+    return str(Song(orderer=Song.orderers[rnd](), formatter=Song.formatters[double]()))
