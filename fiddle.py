2c4ac4dc610bd1f6065c70d8708f6e2f33b0c4fa
igrekus <upload234@mail.ru>
2020-05-10 15:57:18 +0300

    REF: extract functions from class to module-level

--- fiddle.py
+++ fiddle.py
@@ -17,43 +17,51 @@ def _id(x): return x
 orders = {True: _random_order, False: _id}
 formats = {True: _double_format, False: _id}
 
+data = [
+    'the horse and the hound and the horn,\nThat belong to ',
+    'the farmer sowing his corn,\nThat kept ',
+    'the rooster that crow\'d in the morn,\nThat waked ',
+    'the priest all shaven and shorn,\nThat married ',
+    'the man all tattered and torn,\nThat kissed ',
+    'the maiden all forlorn,\nThat milked ',
+    'the cow with the crumpled horn,\nThat tossed ',
+    'the dog,\nThat worried ',
+    'the cat,\nThat killed ',
+    'the rat,\nThat ate ',
+    'the malt\nThat lay in ',
+    'the house that Jack built '
+]
+
+
+def _parts(data, format, num):
+    return format(data[-num:])
+
+
+def _phrase(data, format, num):
+    return ''.join(_parts(data, format, num))
+
+
+def _line(data, format, num):
+    return f'This is {_phrase(data, format, num).strip()}.'
+
+
+order = None
+format = None
+
 
 class Song:
-
-    def __init__(self, order, format):
-        self.data = [
-            'the horse and the hound and the horn,\nThat belong to ',
-            'the farmer sowing his corn,\nThat kept ',
-            'the rooster that crow\'d in the morn,\nThat waked ',
-            'the priest all shaven and shorn,\nThat married ',
-            'the man all tattered and torn,\nThat kissed ',
-            'the maiden all forlorn,\nThat milked ',
-            'the cow with the crumpled horn,\nThat tossed ',
-            'the dog,\nThat worried ',
-            'the cat,\nThat killed ',
-            'the rat,\nThat ate ',
-            'the malt\nThat lay in ',
-            'the house that Jack built '
-        ]
+    def __init__(self, data, order, format):
         self._order = order
         self._format = format
-        self.data = self._order(self.data)
+        self.data = data
 
     def __str__(self):
-        return '\n\n'.join(self._line(i) for i in range(1, len(self) + 1))
-
-    def _line(self, num):
-        return f'This is {self._phrase(num).strip()}.'
-
-    def _parts(self, num):
-        return self._format(self.data[-num:])
-
-    def _phrase(self, num):
-        return ''.join(self._parts(num))
-
-    def __len__(self):
-        return self.data.__len__()
+        return '\n\n'.join(_line(self.data, format, i) for i in range(1, len(self.data) + 1))
 
 
 def song(rnd=False, double=False):
-    return str(Song(order=orders[rnd], format=formats[double]))
+    global order, format, data
+    order = orders[rnd]
+    format = formats[double]
+    d = order(data)
+    return str(Song(data=d, order=order, format=format))
