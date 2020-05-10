2b06db7a65d567dbb79eea8f2cf94c9761f2505f
igrekus <upload234@mail.ru>
2020-05-10 17:12:41 +0300

    REF: remove old helper functions

--- fiddle.py
+++ fiddle.py
@@ -1,20 +1,6 @@
 import itertools
 import random
 
-
-def _random_order(data):
-    return sorted(data, key=lambda x: random.randint(0, 100))
-
-
-def _double_format(parts): return itertools.chain(*[[lhs, rhs] for lhs, rhs in zip(parts, parts)])
-
-
-def _id(x): return x
-
-
-orders = {True: _random_order, False: _id}
-formats = {True: _double_format, False: _id}
-
 data = [
     'the horse and the hound and the horn,\nThat belong to ',
     'the farmer sowing his corn,\nThat kept ',
@@ -31,20 +17,22 @@ data = [
 ]
 
 
-def _parts(data, format, num):
-    return format(data[-num:])
+def _random(it):
+    return sorted(it, key=lambda x: random.randint(0, 100))
 
 
-def _phrase(data, format, num):
-    return ''.join(_parts(data, format, num))
+def _double(it): return itertools.chain(*[[lhs, rhs] for lhs, rhs in zip(it, it)])
 
 
-def _line(data, format, num):
-    return f'This is {_phrase(data, format, num).strip()}.'
+def _id(x): return x
+
+
+orders = {True: _random, False: _id}
+formats = {True: _double, False: _id}
 
 
 def _song(data, format):
-    return '\n\n'.join(_line(data, format, i) for i in range(1, len(data) + 1))
+    return '\n\n'.join(f'This is {"".join(format(data[-i:])).strip()}.' for i in range(1, len(data) + 1))
 
 
 def song(rnd=False, double=False):
