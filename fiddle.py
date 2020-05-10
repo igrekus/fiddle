2a101aac64d3f4a788fbd1754916b696ff4a6c83
igrekus <upload234@mail.ru>
2020-05-10 15:37:36 +0300

    REF: make order and format into functions

--- fiddle.py
+++ fiddle.py
@@ -2,33 +2,25 @@ import itertools
 import random
 
 
-class DefaultOrder:
-    def order(self, data):
-        return data
+def _random_order(data):
+    d = list(data)
+    random.shuffle(d)
+    return d
 
 
-class RandomOrder:
-    def order(self, data):
-        d = list(data)
-        random.shuffle(d)
-        return d
+def _double_format(parts): return itertools.chain(*[[lhs, rhs] for lhs, rhs in zip(parts, parts)])
 
 
-class DefaultFormat:
-    def format(self, parts):
-        return parts
+def _id(x): return x
 
 
-class DoubleFormat:
-    def format(self, parts):
-        return itertools.chain(*[[l, r] for l, r in zip(parts, parts)])
+orders = {True: _random_order, False: _id}
+formats = {True: _double_format, False: _id}
 
 
 class Song:
-    orderers = {True: RandomOrder, False: DefaultOrder}
-    formatters = {True: DoubleFormat, False: DefaultFormat}
 
-    def __init__(self, orderer, formatter):
+    def __init__(self, order, format):
         self.data = [
             'the horse and the hound and the horn,\nThat belong to ',
             'the farmer sowing his corn,\nThat kept ',
@@ -43,9 +35,9 @@ class Song:
             'the malt\nThat lay in ',
             'the house that Jack built '
         ]
-        self._orderer = orderer
-        self._formatter = formatter
-        self.data = self._orderer.order(self.data)
+        self._order = order
+        self._format = format
+        self.data = self._order(self.data)
 
     def __str__(self):
         return '\n\n'.join(self._line(i) for i in range(1, len(self) + 1))
@@ -54,7 +46,7 @@ class Song:
         return f'This is {self._phrase(num).strip()}.'
 
     def _parts(self, num):
-        return self._formatter.format(self.data[-num:])
+        return self._format(self.data[-num:])
 
     def _phrase(self, num):
         return ''.join(self._parts(num))
@@ -64,4 +56,4 @@ class Song:
 
 
 def song(rnd=False, double=False):
-    return str(Song(orderer=Song.orderers[rnd](), formatter=Song.formatters[double]()))
+    return str(Song(order=orders[rnd], format=formats[double]))
