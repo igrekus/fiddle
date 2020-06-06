6c25acc19868697e614de1c8c9caf95a1d512d44
igrekus <upload234@mail.ru>
2020-06-06 16:04:06 +0300

    REF: simplify recursive function

--- fiddle.py
+++ fiddle.py
@@ -1,17 +1,18 @@
-s1 = '123456789'
-s2 = '123567894'
+s1 = '0123456789'
+s2 = '0123567894'
+
+
+def _is_consequent(one, two):
+    return one < two and two - one == 1
 
 
 def _recur(seq):
-    if len(list(seq)) == 2:
-        first, last = seq
-        return first < last and last - first == 1
-    head, next, *tail = seq
-    return head < next and next - head == 1 and _recur([next] + tail)
+    one, two, *rest = seq
+    return _is_consequent(one, two) and _recur([two] + rest) if rest else _is_consequent(one, two)
 
 
 def check(seq, n: int) -> bool:
-    return _recur(list(map(int, seq)))
+    return _recur(map(int, seq))
 
 
 print(check(s1, 1))
