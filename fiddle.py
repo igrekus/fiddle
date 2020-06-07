67fedf024edd3f7b14b17a30205e4bf2ae5f5b25
igrekus <upload234@mail.ru>
2020-06-07 17:01:39 +0300

    REF: rearrange functions as per Clean code styleguide

--- fiddle.py
+++ fiddle.py
@@ -21,8 +21,15 @@ from itertools import zip_longest
 __all__ = ['ascending']
 
 
-def _is_consequent(one, two):
-    return one < two and two - one == 1
+def ascending(value: str) -> bool:
+    return any(_check(value, i) for i in range(1, len(value) // 2))
+
+
+def _check(seq, n):
+    try:
+        return _recur(map(int, _group(seq, n)))
+    except TypeError:
+        return False
 
 
 def _recur(seq):
@@ -30,12 +37,21 @@ def _recur(seq):
     return _is_consequent(one, two) and _recur([two] + rest) if rest else _is_consequent(one, two)
 
 
+def _is_consequent(one, two):
+    return one < two and two - one == 1
+
+
+def _group(it, n):
+    border = f'8{"9" * n}1'
+    return \
+        _make_groups(_left(it, border), n) + \
+        _make_groups(_right(it, border), n + 1) \
+        if border in it \
+        else _make_groups(it, n)
+
+
 def _make_groups(it, n):
     return [''.join(el) for el in zip_longest(*([iter(it)] * n))]
-
-
-def _index(it, border):
-    return it.index(border) + len(border) - 1
 
 
 def _left(it, border):
@@ -46,17 +62,5 @@ def _right(it, border):
     return it[_index(it, border):]
 
 
-def _group(it, n):
-    border = f'8{"9" * n}1'
-    return _make_groups(_left(it, border), n) + _make_groups(_right(it, border), n + 1) if border in it else _make_groups(it, n)
-
-
-def _check(seq, n):
-    try:
-        return _recur(map(int, _group(seq, n)))
-    except TypeError:
-        return False
-
-
-def ascending(value: str) -> bool:
-    return any(_check(value, i) for i in range(1, len(value) // 2))
+def _index(it, border):
+    return it.index(border) + len(border) - 1
