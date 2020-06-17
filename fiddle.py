690abf9718f89867cceea612639630360cfcc022
igrekus <upload234@mail.ru>
2020-06-17 14:42:27 +0300

    REF: use universal character source

--- fiddle.py
+++ fiddle.py
@@ -4,22 +4,22 @@ from string import ascii_uppercase
 def diamond(letter, background=' '):
     letters = ascii_uppercase[:ascii_uppercase.index(letter) + 1]
     if letter == letters[0]:
-        return 'A'
+        return letters[0]
     elif letter == letters[1]:
-        return ' A \n' \
-               'B B\n' \
-               ' A \n'
+        return f' {letters[0]} \n' \
+               f'{letters[1]} {letters[1]}\n' \
+               f' {letters[0]} \n'
     elif letter == letters[2]:
-        return '  A  \n' \
-               ' B B \n' \
-               'C   C\n' \
-               ' B B \n' \
-               '  A  \n'
+        return f'  {letters[0]}  \n' \
+               f' {letters[1]} {letters[1]} \n' \
+               f'{letters[2]}   {letters[2]}\n' \
+               f' {letters[1]} {letters[1]} \n' \
+               f'  {letters[0]}  \n'
     elif letter == letters[3]:
-        return '   A   \n' \
-               '  B B  \n' \
-               ' C   C \n' \
-               'D     D\n' \
-               ' C   C \n' \
-               '  B B  \n' \
-               '   A   \n'
+        return f'   {letters[0]}   \n' \
+               f'  {letters[1]} {letters[1]}  \n' \
+               f' {letters[2]}   {letters[2]} \n' \
+               f'{letters[3]}     {letters[3]}\n' \
+               f' {letters[2]}   {letters[2]} \n' \
+               f'  {letters[1]} {letters[1]}  \n' \
+               f'   {letters[0]}   \n'
