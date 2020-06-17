060885bedb668ef26e25c9ba9cffc3e5e102fd4b
igrekus <upload234@mail.ru>
2020-06-17 14:53:55 +0300

    REF: construct all lines from the input parameters

--- fiddle.py
+++ fiddle.py
@@ -1,25 +1,26 @@
 from string import ascii_uppercase
 
 
-def diamond(letter, background=' '):
-    letters = ascii_uppercase[:ascii_uppercase.index(letter) + 1]
-    if letter == letters[0]:
-        return letters[0]
-    elif letter == letters[1]:
-        return f' {letters[0]} \n' \
-               f'{letters[1]} {letters[1]}\n' \
-               f' {letters[0]} \n'
-    elif letter == letters[2]:
-        return f'  {letters[0]}  \n' \
-               f' {letters[1]} {letters[1]} \n' \
-               f'{letters[2]}   {letters[2]}\n' \
-               f' {letters[1]} {letters[1]} \n' \
-               f'  {letters[0]}  \n'
-    elif letter == letters[3]:
-        return f'   {letters[0]}   \n' \
-               f'  {letters[1]} {letters[1]}  \n' \
-               f' {letters[2]}   {letters[2]} \n' \
-               f'{letters[3]}     {letters[3]}\n' \
-               f' {letters[2]}   {letters[2]} \n' \
-               f'  {letters[1]} {letters[1]}  \n' \
-               f'   {letters[0]}   \n'
+def diamond(letter, bg=' '):
+    ls = ascii_uppercase[:ascii_uppercase.index(letter) + 1]
+    pad = len(ls)
+    if letter == ls[0]:
+        return ls[0]
+    elif letter == ls[1]:
+        return f'{bg * (pad - 1)}{ls[0]}{bg * (pad - 1)}\n' \
+               f'{ls[1]}{bg * (pad - 1)}{ls[1]}\n' \
+               f'{bg * (pad - 1)}{ls[0]}{bg * (pad - 1)}\n'
+    elif letter == ls[2]:
+        return f'{bg * (pad - 1)}{ls[0]}{bg * (pad - 1)}\n' \
+               f'{bg * (pad - 2)}{ls[1]}{bg * (pad - 2)}{ls[1]}{bg * (pad - 2)}\n' \
+               f'{ls[2]}{bg * pad}{ls[2]}\n' \
+               f'{bg * (pad - 2)}{ls[1]}{bg * (pad - 2)}{ls[1]}{bg * (pad - 2)}\n' \
+               f'{bg * (pad - 1)}{ls[0]}{bg * (pad - 1)}\n'
+    elif letter == ls[3]:
+        return f'{bg * (pad - 1)}{ls[0]}{bg * (pad - 1)}\n' \
+               f'{bg * (pad - 2)}{ls[1]}{bg * (pad - 3)}{ls[1]}{bg * (pad - 2)}\n' \
+               f'{bg * (pad - 3)}{ls[2]}{bg * (pad - 1)}{ls[2]}{bg * (pad - 3)}\n' \
+               f'{ls[3]}{bg * (pad + 1)}{ls[3]}\n' \
+               f'{bg * (pad - 3)}{ls[2]}{bg * (pad - 1)}{ls[2]}{bg * (pad - 3)}\n' \
+               f'{bg * (pad - 2)}{ls[1]}{bg * (pad - 3)}{ls[1]}{bg * (pad - 2)}\n' \
+               f'{bg * (pad - 1)}{ls[0]}{bg * (pad - 1)}\n'
