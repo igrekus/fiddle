3fea77f481a5365ed54e0ca2fbdd189baba891e9
igrekus <upload234@mail.ru>
2020-06-18 20:29:00 +0300

    FEAT: add separate file for diamond kata full-lambda solution

---
+++ diamon_full_lambda.py
@@ -0,0 +1,19 @@
+from functools import partial, reduce
+from itertools import chain
+
+
+def diamond(letter: str, background: str=' '):
+    ls = ''.join(map(chr, range(ord('A'), ord(letter) + 1)))
+
+    _id = lambda x: x
+    _compose = lambda *functions: reduce(lambda f, g: lambda *args: f(g(*args)), functions, _id)
+    _apply = lambda fs, *args: (f(*args) for f in fs)
+    _mirror = lambda seq: seq[-2::-1]
+    _flatten = lambda args: chain(*args)
+    _quarter = lambda i, l: ''.join((background * (len(ls) - i - 1), l, background * i))
+    _line_parts = partial(_apply, (_quarter, _compose(_mirror, _quarter)))
+    _make_halfs = partial(_apply, (_id, _mirror))
+    _make_line = _compose(partial(str.join, ''), _line_parts)
+    _make_pic = _compose(partial(str.join, '\n'), _flatten)
+
+    return _make_pic(_make_halfs([_make_line(i, l) for i, l in enumerate(ls)])) + '\n'
