f6254b168e6c7bb2feffa45f067ac73eff73b738
igrekus <upload234@mail.ru>
2020-06-22 13:11:48 +0300

    REF: rename functions for clarity

--- fiddle.py
+++ fiddle.py
@@ -10,11 +10,12 @@ def diamond(letter: str, background: str=' '):
     _apply_many = lambda fs, *args: (f(*args) for f in fs)
     _mirror = lambda seq: seq[-2::-1]
     _flatten = lambda args: chain(*args)
-    _quarter = lambda i, l: ''.join((background * (len(ls) - i - 1), l, background * i))
-    _line_parts = partial(_apply_many, (_quarter, _compose(_mirror, _quarter)))
-    _make_halfs = partial(_apply_many, (_id, _mirror))
-    _make_line = _compose(partial(str.join, ''), _line_parts)
+
+    _make_quarter = lambda i, l: ''.join((background * (len(ls) - i - 1), l, background * i))
+    _make_line_parts = partial(_apply_many, (_make_quarter, _compose(_mirror, _make_quarter)))
+    _make_line = _compose(partial(str.join, ''), _make_line_parts)
     _make_lines = lambda seq: [_make_line(i, l) for i, l in enumerate(ls)]
+    _make_pic_halfs = partial(_apply_many, (_id, _mirror))
     _make_pic = _compose(partial(str.join, '\n'), _flatten)
 
-    return _make_pic(_make_halfs(_make_lines(ls))) + '\n'
+    return _make_pic(_make_pic_halfs(_make_lines(ls))) + '\n'
