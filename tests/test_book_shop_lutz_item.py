caa92c211c4f46ebffe8d4e0962d891f6e2a5095
igrekus <upload234@mail.ru>
2020-06-23 17:07:52 +0300

    REF: rename test for non-mutating item for clarity

--- tests/test_book_shop_lutz_item.py
+++ tests/test_book_shop_lutz_item.py
@@ -2,7 +2,7 @@ from pyexpect import expect
 from fiddle import BookShop, Item
 
 
-def test_lutz_item_day():
+def test_lutz_before_sell_date():
     shop = BookShop([Item('Марк Лутц, Изучаем Python, 3й том', sell_in=50, quality=80)])
 
     shop.update_quality()
@@ -10,7 +10,7 @@ def test_lutz_item_day():
     expect(shop.items).to_equal([Item('Марк Лутц, Изучаем Python, 3й том', sell_in=50, quality=80)])
 
 
-def test_lutz_item_zero_sell_in():
+def test_lutz_item_on_sell_date():
     shop = BookShop([Item('Марк Лутц, Изучаем Python, 3й том', sell_in=0, quality=80)])
 
     shop.update_quality()
@@ -18,7 +18,7 @@ def test_lutz_item_zero_sell_in():
     expect(shop.items).to_equal([Item('Марк Лутц, Изучаем Python, 3й том', sell_in=0, quality=80)])
 
 
-def test_lutz_item_negative_sell_in():
+def test_lutz_item_after_sell_date():
     shop = BookShop([Item('Марк Лутц, Изучаем Python, 3й том', sell_in=-1, quality=80)])
 
     shop.update_quality()
