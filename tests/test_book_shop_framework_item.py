b912a892acf2de23793f4aa748dccf1d9f2ac8d2
igrekus <upload234@mail.ru>
2020-06-23 17:21:39 +0300

    REF: update test for new framework item

--- tests/test_book_shop_framework_item.py
+++ tests/test_book_shop_framework_item.py
@@ -1,8 +1,11 @@
+import pytest
+
 from pyexpect import expect
 from fiddle import BookShop, Item
 
 
-def test_framework_item_day():
+@pytest.mark.xfail(reason='not implemented')
+def test_framework_item_before_sell_date():
     shop = BookShop([Item('Фреймворк Django', sell_in=50, quality=50)])
 
     shop.update_quality()
@@ -10,7 +13,8 @@ def test_framework_item_day():
     expect(shop.items).to_equal([Item('Фреймворк Django', sell_in=49, quality=48)])
 
 
-def test_framework_item_zero_quality():
+@pytest.mark.xfail(reason='not implemented')
+def test_framework_item_before_sell_date_min_quality():
     shop = BookShop([Item('Фреймворк Django', sell_in=50, quality=0)])
 
     shop.update_quality()
@@ -18,7 +22,8 @@ def test_framework_item_zero_quality():
     expect(shop.items).to_equal([Item('Фреймворк Django', sell_in=49, quality=0)])
 
 
-def test_framework_item_zero_sell_in():
+@pytest.mark.xfail(reason='not implemented')
+def test_framework_item_on_sell_date():
     shop = BookShop([Item('Фреймворк Django', sell_in=0, quality=10)])
 
     shop.update_quality()
@@ -26,9 +31,28 @@ def test_framework_item_zero_sell_in():
     expect(shop.items).to_equal([Item('Фреймворк Django', sell_in=-1, quality=6)])
 
 
-def test_framework_item_zero_sell_in_zero_quality():
+@pytest.mark.xfail(reason='not implemented')
+def test_framework_item_on_sell_date_min_quality():
     shop = BookShop([Item('Фреймворк Django', sell_in=0, quality=0)])
 
     shop.update_quality()
 
     expect(shop.items).to_equal([Item('Фреймворк Django', sell_in=-1, quality=0)])
+
+
+@pytest.mark.xfail(reason='not implemented')
+def test_framework_item_after_sell_date():
+    shop = BookShop([Item('Фреймворк Django', sell_in=-1, quality=10)])
+
+    shop.update_quality()
+
+    expect(shop.items).to_equal([Item('Фреймворк Django', sell_in=-2, quality=6)])
+
+
+@pytest.mark.xfail(reason='not implemented')
+def test_framework_item_after_sell_date_min_quality():
+    shop = BookShop([Item('Фреймворк Django', sell_in=-1, quality=0)])
+
+    shop.update_quality()
+
+    expect(shop.items).to_equal([Item('Фреймворк Django', sell_in=-2, quality=0)])
