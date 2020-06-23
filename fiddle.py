0b2c2ff64d1cd78e14739b7d06eb08c15d4e39da
igrekus <upload234@mail.ru>
2020-06-23 18:57:04 +0300

    REF: remove unneeded branching if

--- fiddle.py
+++ fiddle.py
@@ -64,34 +64,6 @@ class BookShop:
                 self.coupon_tick(item)
                 continue
 
-            if item.name != "Д. Кнут, Искусство программирования" and item.name != "Скидочный купон на курс":
-                if item.quality > 0:
-                    if item.name != "Марк Лутц, Изучаем Python, 3й том":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Скидочный купон на курс":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Марк Лутц, Изучаем Python, 3й том":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Д. Кнут, Искусство программирования":
-                    if item.name != "Скидочный купон на курс":
-                        if item.quality > 0:
-                            if item.name != "Марк Лутц, Изучаем Python, 3й том":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-
     def normal_tick(self, item):
         item.sell_in -= 1
         if item.quality == 0:
