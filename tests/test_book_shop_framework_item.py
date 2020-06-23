d95768fb554c05e211e746f31fe2d2058a14307b
igrekus <upload234@mail.ru>
2020-06-23 19:21:35 +0300

    REF: remove expected-to-fail mark on framework item tests

--- tests/test_book_shop_framework_item.py
+++ tests/test_book_shop_framework_item.py
@@ -4,7 +4,6 @@ from pyexpect import expect
 from fiddle import BookShop, Item
 
 
-@pytest.mark.xfail(reason='not implemented')
 def test_framework_item_before_sell_date():
     shop = BookShop([Item('Фреймворк Django', sell_in=50, quality=50)])
 
@@ -13,7 +12,6 @@ def test_framework_item_before_sell_date():
     expect(shop.items).to_equal([Item('Фреймворк Django', sell_in=49, quality=48)])
 
 
-@pytest.mark.xfail(reason='not implemented')
 def test_framework_item_before_sell_date_min_quality():
     shop = BookShop([Item('Фреймворк Django', sell_in=50, quality=0)])
 
@@ -22,7 +20,6 @@ def test_framework_item_before_sell_date_min_quality():
     expect(shop.items).to_equal([Item('Фреймворк Django', sell_in=49, quality=0)])
 
 
-@pytest.mark.xfail(reason='not implemented')
 def test_framework_item_on_sell_date():
     shop = BookShop([Item('Фреймворк Django', sell_in=0, quality=10)])
 
@@ -31,7 +28,6 @@ def test_framework_item_on_sell_date():
     expect(shop.items).to_equal([Item('Фреймворк Django', sell_in=-1, quality=6)])
 
 
-@pytest.mark.xfail(reason='not implemented')
 def test_framework_item_on_sell_date_min_quality():
     shop = BookShop([Item('Фреймворк Django', sell_in=0, quality=0)])
 
@@ -40,7 +36,6 @@ def test_framework_item_on_sell_date_min_quality():
     expect(shop.items).to_equal([Item('Фреймворк Django', sell_in=-1, quality=0)])
 
 
-@pytest.mark.xfail(reason='not implemented')
 def test_framework_item_after_sell_date():
     shop = BookShop([Item('Фреймворк Django', sell_in=-1, quality=10)])
 
@@ -49,7 +44,6 @@ def test_framework_item_after_sell_date():
     expect(shop.items).to_equal([Item('Фреймворк Django', sell_in=-2, quality=6)])
 
 
-@pytest.mark.xfail(reason='not implemented')
 def test_framework_item_after_sell_date_min_quality():
     shop = BookShop([Item('Фреймворк Django', sell_in=-1, quality=0)])
 
