b026876fc847d22bf056a29b7d76d63e41ec276c
igrekus <upload234@mail.ru>
2020-10-31 16:05:50 +0300

    WIP: implement recursive map

--- fiddle.py
+++ fiddle.py
@@ -2,25 +2,9 @@ from functools import partial
 
 raw = [1, 1, 3, 4, 7, 20]
 
-
-grow = lambda size, foods: \
-    size > foods[0] \
-        if len(foods) == 1 \
-        else (
-            lambda head, *tail:
-                grow(
-                    size + head if size > head else size,
-                    tail
-                )
-        )(*foods)
-
-
-can_win = lambda foods: \
-    list(map(
-        int,
-        map(
-            partial(grow, foods=foods),
-            foods)
-    ))
+grow = lambda player, foods: player > foods[0] if len(foods) == 1 else (
+    lambda head, *tail: grow(player + head if player > head else player, tail))(*foods)
+walk = lambda f, seq: (lambda head, *tail: [f(seq[-1])] if len(seq) == 1 else [f(head)] + walk(f, tail))(*seq)
+can_win = lambda foods: walk(int, walk(partial(grow, foods=foods), foods))
 
 print(can_win(raw))
