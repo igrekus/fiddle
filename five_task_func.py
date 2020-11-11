b0f00e53ed08b3a30aa2056a76a626ce48b34351
igrekus <upload234@mail.ru>
2020-11-11 19:01:47 +0300

    FEAT: add functional solution for the five problems task

---
+++ five_task_func.py
@@ -0,0 +1,34 @@
+import math
+
+from functools import partial
+from itertools import count
+
+
+nearest_bus_stop = lambda k, n: next(min(k * i - n, abs(k * i - k - n)) for i in count() if k * i > n)
+
+
+pack_pastry = lambda a, b:\
+    (a // 3, b // 3) if a - b == 0 else \
+        None if abs(a - b) != 1 else \
+            (lambda com: (com + 1, com) if a - b > 0 else (com, com + 1))(max(a // 3, b // 3))
+
+
+rotate_board = lambda before: \
+    [row.index(1) + 1 for row in list(zip(*reversed([[int(i == (v - 1)) for i in range(len(before))] for v in before])))]
+
+
+locate_number = lambda x: \
+    (lambda side:
+     (lambda dir, start:
+      [(idx + 1, side) if dir else (side, idx + 1) for idx, v in
+       enumerate(range(start, start + side)) if v == x][0]
+      if x <= start + (side ** 2 - start + 1) // 2
+      else
+      [(side, side - idx - 1) if dir else (side - idx - 1, side) for idx, v in
+       enumerate(range(start + side, side ** 2 + 1)) if v == x][0])
+     (side % 2 == 0, (side - 1) ** 2 + 1))(math.ceil(math.sqrt(x)))
+
+
+grow = lambda p, fs: p[1] > fs[0][1] if len(fs) == 1 else \
+    (lambda head, *tail: grow((p[0], p[1] + head[1]) if p[1] > head[1] and p[0] != head[0] else p, tail))(*fs)
+find_winners = lambda pls: list((lambda ps: map(int, map(partial(grow, fs=ps), ps)))(list(enumerate(pls))))
