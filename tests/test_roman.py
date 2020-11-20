a1a01314adabc40fb0e8a67815af7ed9d83b9b7e
igrekus <upload234@mail.ru>
2020-11-20 17:37:09 +0300

    TEST: replace parametrized test with a loop

--- tests/test_roman.py
+++ tests/test_roman.py
@@ -29,6 +29,6 @@ def test_thousands(act, exp):
     expect(to_roman(act)).to_equal(exp)
 
 
-@pytest.mark.parametrize('act,exp', romans)
-def test_all(act, exp):
-    expect(to_roman(act)).to_equal(exp)
+def test_all_():
+    for act, exp in romans:
+        expect(to_roman(act)).to_equal(exp)
