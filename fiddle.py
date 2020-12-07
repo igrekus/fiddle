3fe6036c8bc5259544763a00b258e5c746169d77
igrekus <upload234@mail.ru>
2020-12-07 17:32:41 +0300

    FEAT: implement functional recursive solution

--- fiddle.py
+++ fiddle.py
@@ -1,16 +1,19 @@
 import re
 
-_digit = {
-    **dict.fromkeys('123', lambda n, unit, *_: unit * int(n)),
-    **dict.fromkeys('45', lambda n, unit, half, *_: unit * (5 - int(n)) + half),
-    **dict.fromkeys('678', lambda n, unit, half, *_: half + unit * (int(n) - 5)),
-    '9': lambda _, unit, half, next: unit + next
-}
+_get = (
+    lambda f, *args: lambda *a: f(*(a + args)))(
+    {
+        **dict.fromkeys('123', lambda n, unit, *_: unit * int(n)),
+        **dict.fromkeys('45', lambda n, unit, half, *_: unit * (5 - int(n)) + half),
+        **dict.fromkeys('678', lambda n, unit, half, *_: half + unit * (int(n) - 5)),
+        '9': lambda _, unit, half, next: unit + next
+    }.get,
+    lambda *_: ''
+)
 
-
-def to_roman(n: int) -> str:
-    return ''.join(
-        _digit.get(v, lambda *_: '')(v, *units)
+to_roman = lambda n: \
+    ''.join(
+        _get(v)(v, *units)
         for v, units in zip(
             f'{n:04d}',
             [['M', 'V̅', 'X̅'],
@@ -29,17 +32,18 @@ pairs = list(zip(
 roman_re = re.compile(r'^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$')
 
 
-def _parse_digit(total, digit, value, rest):
-    return _parse_digit(total + value, digit, value, rest[len(digit):]) if rest.startswith(digit) else (total, rest)
+_parse_digit = \
+    lambda total, digit, value, rest: \
+        _parse_digit(total + value, digit, value, rest[len(digit):]) if rest.startswith(digit) else (total, rest)
+
+_recur = \
+    lambda total, roman, ps: \
+        (total, '') if len(ps) == 0 else _recur(*(lambda t, r: _parse_digit(t, *ps[0], r))(total, roman), ps[1:])
 
 
-def parse_roman(roman):
-    if not roman or not roman_re.match(roman):
-        return -1
-    total = 0
-    for r, a in pairs:
-        total, roman = _parse_digit(total, r, a, roman)
-    return total
+parse_roman = \
+    lambda roman: \
+        -1 if not roman or not roman_re.match(roman) else _recur(0, roman, pairs)[0]
 
 
 if __name__ == '__main__':
