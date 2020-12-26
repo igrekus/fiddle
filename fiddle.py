445cebbebaecb2564521a2b21aea43147154a16f
igrekus <upload234@mail.ru>
2020-12-26 20:09:32 +0300

    WIP: implement recursive circle-dots solution

--- fiddle.py
+++ fiddle.py
@@ -1,2 +1,5 @@
-def count_dots(radius):
-    return sum(2 * int((radius ** 2 - x ** 2) ** 0.5) + 1 for x in range(-int(radius), int(radius) + 1))
+count_dots = lambda radius:  _loop(radius, -int(radius), int(radius))
+
+_loop = \
+    lambda r, i, t: \
+        2 * int((r ** 2 - i ** 2) ** 0.5) + 1 + _loop(r, i + 1, t) if i <= t else 0
