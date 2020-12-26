e95b2c51ab091e9412fe4cae4e5df38dc25a552b
igrekus <upload234@mail.ru>
2020-12-26 20:09:56 +0300

    MICS: move recursive circle-dots solution to a separate file

---
+++ circle_dots_rec.py
@@ -0,0 +1,5 @@
+count_dots = lambda radius:  _loop(radius, -int(radius), int(radius))
+
+_loop = \
+    lambda r, i, t: \
+        2 * int((r ** 2 - i ** 2) ** 0.5) + 1 + _loop(r, i + 1, t) if i <= t else 0
