2ebd252953a494005bdf6164c18b3e0a67efdf76
igrekus <upload234@mail.ru>
2021-01-25 00:07:59 +0300

    WIP: add solution to the second stage

--- jack_song_perf0mance_artist.py
+++ jack_song_perf0mance_artist.py
@@ -1,90 +1,57 @@
+def generate_double_couplet(n):
+    plot = ['That lay in', 'That ate', 'That killed', 'That worried',
+    'That tossed', 'That milked', 'That kissed', 'That married',
+    'That waked', 'That kept', 'That belong to']
+
+    actors = ['the house that Jack built', 'the malt', 'the rat,',
+    'the cat,', 'the dog,', 'the cow with the crumpled horn,',
+    'the maiden all forlorn,', 'the man all tattered and torn,', 'the priest all shaven and shorn,',
+    'the rooster that crow\'d in the morn,', 'the farmer sowing his corn,', 'the horse and the hound and the horn,']
+
+    couplet = f'This is {actors[n]}'
+
+    if n == 0:
+        couplet = f'This is {actors[n]} {actors[n]}'
+
+    if n+1 > 1:
+        for k in range(n-1, 0, -1):
+            couplet = f'{couplet}\n{plot[k]} {actors[k+1]}\n{plot[k]} {actors[k]}'
+
+        couplet = f'{couplet}\n{plot[0]} {actors[1]}\n{plot[0]} {actors[0]} {actors[0]}'
+
+    return f'{couplet}.'
+
+def double_song():
+    song = []
+
+    for i in range(12):
+        song.append(generate_double_couplet(i))
+        
+    return '\n\n'.join(song)
+
+def generate_couplet(n):
+    plot = ['That lay in', 'That ate', 'That killed', 'That worried',
+    'That tossed', 'That milked', 'That kissed', 'That married',
+    'That waked', 'That kept', 'That belong to']
+
+    actors = ['the house that Jack built', 'the malt', 'the rat,',
+    'the cat,', 'the dog,', 'the cow with the crumpled horn,',
+    'the maiden all forlorn,', 'the man all tattered and torn,', 'the priest all shaven and shorn,',
+    'the rooster that crow\'d in the morn,', 'the farmer sowing his corn,', 'the horse and the hound and the horn,']
+
+    couplet = f'This is {actors[n]}'
+
+    for k in range(n, 0, -1):
+        couplet = f'{couplet}\n{plot[k-1]} {actors[k-1]}'
+
+    return f'{couplet}.'
+
 def song():
-    return """This is the house that Jack built.
+    song = []
 
-This is the malt
-That lay in the house that Jack built.
+    for i in range(12):
+        song.append(generate_couplet(i))
+        
+    return '\n\n'.join(song)
 
-This is the rat,
-That ate the malt
-That lay in the house that Jack built.
-
-This is the cat,
-That killed the rat,
-That ate the malt
-That lay in the house that Jack built.
-
-This is the dog,
-That worried the cat,
-That killed the rat,
-That ate the malt
-That lay in the house that Jack built.
-
-This is the cow with the crumpled horn,
-That tossed the dog,
-That worried the cat,
-That killed the rat,
-That ate the malt
-That lay in the house that Jack built.
-
-This is the maiden all forlorn,
-That milked the cow with the crumpled horn,
-That tossed the dog,
-That worried the cat,
-That killed the rat,
-That ate the malt
-That lay in the house that Jack built.
-
-This is the man all tattered and torn,
-That kissed the maiden all forlorn,
-That milked the cow with the crumpled horn,
-That tossed the dog,
-That worried the cat,
-That killed the rat,
-That ate the malt
-That lay in the house that Jack built.
-
-This is the priest all shaven and shorn,
-That married the man all tattered and torn,
-That kissed the maiden all forlorn,
-That milked the cow with the crumpled horn,
-That tossed the dog,
-That worried the cat,
-That killed the rat,
-That ate the malt
-That lay in the house that Jack built.
-
-This is the rooster that crow'd in the morn,
-That waked the priest all shaven and shorn,
-That married the man all tattered and torn,
-That kissed the maiden all forlorn,
-That milked the cow with the crumpled horn,
-That tossed the dog,
-That worried the cat,
-That killed the rat,
-That ate the malt
-That lay in the house that Jack built.
-
-This is the farmer sowing his corn,
-That kept the rooster that crow'd in the morn,
-That waked the priest all shaven and shorn,
-That married the man all tattered and torn,
-That kissed the maiden all forlorn,
-That milked the cow with the crumpled horn,
-That tossed the dog,
-That worried the cat,
-That killed the rat,
-That ate the malt
-That lay in the house that Jack built.
-
-This is the horse and the hound and the horn,
-That belong to the farmer sowing his corn,
-That kept the rooster that crow'd in the morn,
-That waked the priest all shaven and shorn,
-That married the man all tattered and torn,
-That kissed the maiden all forlorn,
-That milked the cow with the crumpled horn,
-That tossed the dog,
-That worried the cat,
-That killed the rat,
-That ate the malt
-That lay in the house that Jack built."""
+print(song())
