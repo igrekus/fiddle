bfc6118511d6d0f004aa002fc94d86c38a5255c6
igrekus <upload234@mail.ru>
2021-01-24 22:30:37 +0300

    WIP: add solutions to the second stage

--- jack_song_denis.py
+++ jack_song_denis.py
@@ -1,93 +1,46 @@
-song_text = """This is the house that Jack built.
-
-This is the malt
-That lay in the house that Jack built.
-
-This is the rat,
-That ate the malt
-That lay in the house that Jack built.
-
-This is the cat,
-That killed the rat,
-That ate the malt
-That lay in the house that Jack built.
-
-This is the dog,
-That worried the cat,
-That killed the rat,
-That ate the malt
-That lay in the house that Jack built.
-
-This is the cow with the crumpled horn,
-That tossed the dog,
-That worried the cat,
-That killed the rat,
-That ate the malt
-That lay in the house that Jack built.
-
-This is the maiden all forlorn,
-That milked the cow with the crumpled horn,
-That tossed the dog,
-That worried the cat,
-That killed the rat,
-That ate the malt
-That lay in the house that Jack built.
-
-This is the man all tattered and torn,
-That kissed the maiden all forlorn,
-That milked the cow with the crumpled horn,
-That tossed the dog,
-That worried the cat,
-That killed the rat,
-That ate the malt
-That lay in the house that Jack built.
-
-This is the priest all shaven and shorn,
-That married the man all tattered and torn,
-That kissed the maiden all forlorn,
-That milked the cow with the crumpled horn,
-That tossed the dog,
-That worried the cat,
-That killed the rat,
-That ate the malt
-That lay in the house that Jack built.
-
-This is the rooster that crow'd in the morn,
-That waked the priest all shaven and shorn,
-That married the man all tattered and torn,
-That kissed the maiden all forlorn,
-That milked the cow with the crumpled horn,
-That tossed the dog,
-That worried the cat,
-That killed the rat,
-That ate the malt
-That lay in the house that Jack built.
-
-This is the farmer sowing his corn,
-That kept the rooster that crow'd in the morn,
-That waked the priest all shaven and shorn,
-That married the man all tattered and torn,
-That kissed the maiden all forlorn,
-That milked the cow with the crumpled horn,
-That tossed the dog,
-That worried the cat,
-That killed the rat,
-That ate the malt
-That lay in the house that Jack built.
-
-This is the horse and the hound and the horn,
-That belong to the farmer sowing his corn,
-That kept the rooster that crow'd in the morn,
-That waked the priest all shaven and shorn,
-That married the man all tattered and torn,
-That kissed the maiden all forlorn,
-That milked the cow with the crumpled horn,
-That tossed the dog,
-That worried the cat,
-That killed the rat,
-That ate the malt
-That lay in the house that Jack built."""
+introduction = "This is"
+snippets = {"That lay in": " the house that Jack built.\n",
+            "That ate": " the malt\n",
+            "That killed": " the rat,\n",
+            "That worried": " the cat,\n",
+            "That tossed": " the dog,\n",
+            "That milked": " the cow with the crumpled horn,\n",
+            "That kissed": " the maiden all forlorn,\n",
+            "That married": " the man all tattered and torn,\n",
+            "That waked": " the priest all shaven and shorn,\n",
+            "That kept": " the rooster that crow'd in the morn,\n",
+            "That belong to": " the farmer sowing his corn,\n",
+            "This is": " the horse and the hound and the horn,\n"}
 
 
 def song():
-    return song_text
+    actions = list(snippets.keys())
+    stories = list(snippets.values())
+    all_song = ""
+    for rows in range(1, len(stories) + 1):
+        couplet = ""
+        for row in range(rows):
+            if row == rows - 1:
+                couplet = introduction + stories[row] + couplet
+            else:
+                couplet = actions[row] + stories[row] + couplet
+        all_song += couplet + ("\n" if rows != len(stories) else "")
+    return all_song.strip()
+
+
+def double_song():
+    actions = [""] + list(snippets.keys())
+    stories = list(snippets.values())
+    stories[0] = " the house that Jack built"
+    all_song = ""
+    for rows in range(1, len(stories) + 1):
+        couplet = ""
+        for row in range(rows):
+            if row == rows - 1:
+                couplet = introduction + stories[row] + actions[row] + stories[row] + couplet
+            else:
+                couplet = actions[row + 1] + stories[row] + actions[row] + stories[row] + couplet
+            if row == rows - 1:
+                couplet += ".\n"
+        all_song += couplet + ("\n" if rows != len(stories) else "")
+    return all_song.strip()
--- jack_song_pokemon.py
+++ jack_song_pokemon.py
@@ -14,10 +14,25 @@ That ate the malt
 That lay in the house that Jack built."""
 
 
-def song() -> str:
-    make_verse = lambda n, v: (lambda f: "\n".join(f(v[n]) + v[n + 1:]))(lambda s: [f"This is {s[s.find('the'):]}"])
+def song(verse=verse) -> str:
+    make_verse = lambda n, v: "This is " + (lambda s: s[s.find("the"):])("\n".join(v[n:]))
     return (lambda v: "\n\n".join(map(partial(make_verse, v=v), range(len(v))[::-1])))(verse.split("\n"))
 
 
-if __name__ == '__main__':
-    print(song())
+def double_song(verse=verse) -> str:
+    _last_row = "the house that Jack built the house that Jack built."
+    _conc = lambda s, n: (lambda f: f"{s[n][f(s[n]):]}\n{s[n + 1][:f(s[n + 1])]}")(lambda c: c.find("the"))
+    make_verse = lambda n, v: n == len(v) - 1 and _last_row or 2 * (_conc(v, n)) + make_verse(n + 1, v)
+    return (
+        lambda v: "\n\n".join(map(lambda *args: "This is " + partial(make_verse, v=v)(*args), range(len(v))[::-1])))(
+        verse.split("\n"))
+
+
+def double_song_(verse=verse) -> str:
+    # без хардкодинга последней строки, но тогда в ней пролезает лишняя точка
+    _conc = lambda s, n: (lambda f: s[n][f(s[n]):] + (f"\n{s[n + 1][:f(s[n + 1])]}" if n < len(s) - 1 else ""))(
+        lambda c: c.find("the"))
+    make_verse = lambda n, v: "" if n == len(v) else 2 * (_conc(v, n)) + make_verse(n + 1, v)
+    return (
+        lambda v: "\n\n".join(map(lambda *args: "This is " + partial(make_verse, v=v)(*args), range(len(v))[::-1])))(
+        verse.split("\n"))
