32b17baabfd7ffca2535ab7aeb3bfaa302f6a9ac
igrekus <upload234@mail.ru>
2021-02-12 18:33:03 +0300

    WIP: fix solution

--- jack_song_AmigoSP.py
+++ jack_song_AmigoSP.py
@@ -2,9 +2,8 @@ import random
 import inspect
 import logging
 
-
 logging.basicConfig(format='%(message)s')
-log = logging.getLogger()
+log = logging.getLogger(__name__)
 
 
 def who_called(func):
@@ -129,6 +128,32 @@ def _get_path_song(chunk: int):
 
 
 @who_called
+def double_song():
+    chunk_list = list(range(0, 12))
+    counter_cuplet = 12
+    _text = []
+    prev_text = ''
+    double_random = hasattr(song, '_is_random')
+    for _ in range(1, counter_cuplet + 1):
+        if not double_random:
+            name, actions = _get_path_song(_ - 1)
+        else:
+            chunk_random = random.choice(chunk_list)
+            chunk_list.remove(chunk_random)
+            name, actions = _get_path_song(chunk_random)
+        if _ == 1 and not actions:
+            _text.append(f"This is the {name} the {name}.")
+        elif _ == 1 and actions:
+            _text.append(f"This is the {name}\nThat {actions} the {name}\nThat {actions}.")
+        elif actions:
+            _text.append(f"This is the {name}\nThat {actions} the {name}\nThat {actions} the {prev_text}")
+        else:
+            _text.append(f"This is the {name} the {name} the {prev_text}")
+        prev_text = _text[-1].replace('This is the ', '')
+    return '\n\n'.join(_text)
+
+
+@who_called
 def random_song():
     chunk_list = list(range(0, 12))
     counter_cuplet = 12
@@ -165,50 +190,6 @@ def song(rnd=False, double=False):
         return ORIGINAL_SONG
 
 
-def _random_and_double(text_song: str) -> str:
-    new_text = []
-    check_chunk = 'the house that Jack built'
-    new_chunk = 'the house that Jack built the house that Jack built'
-    for chunk in text_song.split('\n'):
-        new_text.append(chunk.replace(check_chunk, new_chunk))
-    return '\n'.join(new_text).strip()
-
-
-def _adapting_to_4_task(container: list) -> str:
-    if container[-1] != '.':
-        text = ''.join(container).replace('.', '') + ' ' + container[-1] + '\n\n'
-    else:
-        text = ''.join(container).replace(' .', '.') + '\n\n'
-    return text
-
-
-@who_called
-def double_song():
-    _is_random = hasattr(song, '_is_random')
-    if not _is_random:
-        song_text = song().split('\n')
-    else:
-        song_text = random_song().split('\n')
-    double_song_text = ''
-    container = []
-    for text_string in song_text:
-        part_one = text_string.find('the')
-        if not text_string:
-            double_song_text += _adapting_to_4_task(container)
-            container = []
-        elif not container:
-            container = [text_string[:part_one], text_string[part_one:]]
-        else:
-            double_song_text += ''.join(container).replace('.', '') + '\n'
-            if part_one >= 0:
-                container[0], next_value = text_string[:part_one], text_string[part_one:]
-            else:
-                container[0], next_value = f'{text_string[:part_one]} ', text_string[part_one:]
-            double_song_text += ''.join(container).replace('.', '') + '\n'
-            container[1] = next_value
-    double_song_text += _adapting_to_4_task(container).strip()
-    return double_song_text if not _is_random else _random_and_double(double_song_text)
-
-
 if __name__ == '__main__':
+    print(song(True, True))
     print(double_song())
