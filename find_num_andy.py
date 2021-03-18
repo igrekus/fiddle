2cfaebc8e340f2de69f742d73a2164713579b29f
igrekus <upload234@mail.ru>
2021-03-18 19:01:21 +0300

    WIP: add solution

---
+++ find_num_andy.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+def find_missing(seq: List[int], n: int) -> int:
+    return sum(range(1, n + 2)) - sum(seq)
+
+
+def find_duplicate(seq: List[int], n: int) -> int:
+    return sum(seq) - sum(range(1, n))
