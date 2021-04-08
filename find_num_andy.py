6773a07b323f36b6aeff11d192be4d073f54f24d
igrekus <upload234@mail.ru>
2021-04-08 18:03:03 +0300

    MISC: clean up, remove old solutions

--- find_num_andy.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-
-def find_missing(seq: List[int], n: int) -> int:
-    return sum(range(1, n + 1)) - sum(seq)
-
-
-def find_duplicate(seq: List[int], n: int) -> int:
-    return sum(seq) - sum(range(1, n + 1))
--- find_num_perf0rmance_artist2.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-def find_difference(seq: List[int], n: int):
-    return abs(sum(seq) - (n**2 + n)//2)
-
-find_duplicate = find_missing = find_difference
-
-#Solution using lamba function
-#find_difference: Callable[[List[int], int], int] = lambda seq, n: abs(sum(seq) - (n**2 + n)//2) 
