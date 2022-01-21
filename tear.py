94a172c4baa0a78a2e4f21bfde89bbb8ada9eee5
igrekus <upload234@mail.ru>
2022-01-21 18:49:17 +0300

    feat: use more correct filter algorithm

--- tear.py
+++ tear.py
@@ -8,7 +8,7 @@ from pathlib import Path
 
 from forgot_again.file import make_dirs
 
-path = r'D:\work\python\jupyter_notebooks\tear_plots\data'
+path = r'D:\work\python\nb\tear_plots\data'
 
 
 def _get_dirs(path):
@@ -34,6 +34,10 @@ def _parse_file(path, folder):
         df = pd.read_csv(bs, sep=';')
         df.columns = ['F(N)', 'L(mm)']
         df = df[df['L(mm)'] != 0]
+
+        df = pd.concat([pd.DataFrame([{'F(N)': 0, 'L(mm)': 0}]), df], ignore_index=True)
+        df.reset_index()
+
         df['measure'] = int(measure)
         df['batch'] = batch
         df['fdiff'] = df['F(N)'].diff()
@@ -41,9 +45,9 @@ def _parse_file(path, folder):
         df['deriv'] = df['fdiff'] / df['ldiff']
         df.to_excel(f'{folder}/{measure}.xlsx', engine='openpyxl', index=False)
 
-    ax = sb.lineplot(data=df, x='L(mm)', y='F(N)', label='raw', ax=None)
-    ax.set(xlim=(0, 4))
-    fig = ax.get_figure()
+    # ax = sb.lineplot(data=df, x='L(mm)', y='F(N)', label='raw', ax=None)
+    # ax.set(xlim=(0, 4))
+    # fig = ax.get_figure()
     # fig.savefig(f'{folder}/{measure}.png', dpi=200)
     # fig.clf()
     return df
@@ -56,10 +60,19 @@ def filter_df(df, folder):
     measure = df['measure'][0]
     batch = df['batch'][0]
     tmp = []
-    for f, d in zip(fs, ds):
+
+    prev = ls[0]
+    remove_indexes = set()
+    for i, (f, d) in enumerate(zip(fs, ds)):
         if d == 0:
+            delta = ls[i] - prev
+            for j in range(i, len(ls)):
+                ls[j] -= delta
+            remove_indexes.add(i)
             continue
         tmp.append([f, d])
+        prev = ls[i]
+    ls = [el for i, el in enumerate(ls) if i not in remove_indexes]
 
     res = [[l, *r] for l, r in zip(ls, tmp)]
     out_df = pd.DataFrame(res, columns=['L(mm)', 'F(N)', 'deriv'])
@@ -67,11 +80,11 @@ def filter_df(df, folder):
     out_df['measure'] = measure
     out_df.to_excel(f'{folder}/{measure:08d}_filtered.xlsx', engine='openpyxl', index=False)
 
-    ax = sb.lineplot(data=out_df, x='L(mm)', y='F(N)', label='filtered', ax=None)
-    ax.set(xlim=(0, 4))
-    fig = ax.get_figure()
-    fig.savefig(f'{folder}/{measure:08d}.png', dpi=200)
-    fig.clf()
+    # ax = sb.lineplot(data=out_df, x='L(mm)', y='F(N)', label='filtered', ax=None)
+    # ax.set(xlim=(0, 4))
+    # fig = ax.get_figure()
+    # fig.savefig(f'{folder}/{measure:08d}.png', dpi=200)
+    # fig.clf()
     return out_df
 
 
@@ -102,3 +115,46 @@ for d in list(_get_dirs(path)):
 
 master.to_excel(f'master.xlsx', engine='openpyxl', index=False)
 master_filtered.to_excel(f'master_filtered.xlsx', engine='openpyxl', index=False)
+
+# def filt(df):
+#     fs = df['F(N)'].values.tolist()
+#     ls = df['L(mm)'].values.tolist()
+#     ds = df['deriv'].values.tolist()
+#     measure = df['measure'][0]
+#     batch = df['batch'][0]
+#     tmp = []
+#
+#     prev = ls[0]
+#     remove_indexes = []
+#     for i, (f, d) in enumerate(zip(fs, ds)):
+#         if d == 0:
+#             delta = ls[i] - prev
+#             for j in range(i, len(ls)):
+#                 ls[j] -= delta
+#             remove_indexes.append(i)
+#             continue
+#         tmp.append([f, d])
+#         prev = ls[i]
+#
+#     remove_indexes = set(remove_indexes)
+#     ls = [el for i, el in enumerate(ls) if i not in remove_indexes]
+#
+#     res = [[l, *r] for l, r in zip(ls, tmp)]
+#     out_df = pd.DataFrame(res, columns=['L(mm)', 'F(N)', 'deriv'])
+#     out_df['batch'] = batch
+#     out_df['measure'] = measure
+#     return out_df
+#
+# df = pd.read_excel(r'D:\work\python\fiddle\00000369.xlsx')
+#
+# f = filt(df)
+#
+# f.to_excel('out.xlsx', index=False)
+#
+# ax = sb.lineplot(data=f, x='L(mm)', y='F(N)', label='filtered', ax=None)
+# sb.lineplot(data=df, x='L(mm)', y='F(N)', label='filtered', ax=None)
+# ax.set(xlim=(0, 4))
+# fig = ax.get_figure()
+# fig.savefig(f'out.png', dpi=200)
+# fig.clf()
+# print(f)
