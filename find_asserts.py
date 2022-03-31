a345498b33f3fee819b956473a82446d1caed46a
igrekus <upload234@mail.ru>
2022-03-31 18:27:56 +0300

    ref: basic refactors

--- find_asserts.py
+++ find_asserts.py
@@ -1,33 +1,49 @@
 from os import listdir
 from pathlib import Path
 
-user = 'test'
-repo_name = 'repo'
-to_repo = f'/home/{user}/source/{repo_name}/'
-targets = ['conftest.py', '__init__.py']
-
-with open('+a-p.txt', mode='rt', encoding='utf-8') as f:
-    paths = [Path(to_repo + line[1:]).absolute() for line in map(str.strip, f.readlines())]
-
 
 def scan(path):
+    if path.parts[-1].startswith('test_'):
+        return []
+
     test_path = path.parent
     candidates = []
     while not str(test_path).endswith(repo_name):
-        targets_full = [_ for _ in listdir(test_path) if _ in targets]
-        if targets_full:
-            targets_full = [str(test_path) + f'/{_}' for _ in targets_full]
-            for target in targets_full:
-                with open(target, mode='rt', encoding='utf-8') as f:
-                    if 'pytest_rewrite_assert' in ''.join(f.readlines()):
-                        break
-                    candidates.append(target)
+        candidates += _check_candidates(_build_full_paths(test_path))
         test_path = test_path.parent
 
-    if candidates:
-        candidates = '\n   '.join(candidates)
-        print(f'\nadd rewrite for: {path}\ncandidates:\n   {candidates}')
+    return candidates
 
 
-for p in paths:
-    scan(p)
+def _check_candidates(files):
+    return filter(bool, (_check_file_for_rewrite(file) for file in files))
+
+
+def _build_full_paths(test_path):
+    return (f'{test_path}/{_}' for _ in _scan_dir_for_targets(test_path))
+
+
+def _check_file_for_rewrite(file):
+    with open(file, mode='rt', encoding='utf-8') as f:
+        return '' if 'pytest_rewrite_assert' in ''.join(f.readlines()) else file
+
+
+def _scan_dir_for_targets(_dir):
+    return filter(lambda file: file in target_files, listdir(_dir))
+
+
+if __name__ == '__main__':
+    user = 'test'
+    repo_name = 'repo'
+    to_repo = f'/home/{user}/source/{repo_name}/'
+    target_files = ['conftest.py', '__init__.py']
+
+    with open(f'./+a-p.txt', mode='rt', encoding='utf-8') as f:
+        paths = [Path(to_repo + line[1:]).absolute() for line in map(str.strip, f.readlines())]
+
+    for p in paths:
+        cs = scan(p)
+        if cs:
+            cs = '\n   '.join(cs)
+            print(f'\nadd rewrite for: {p}\ncandidates:\n   {cs}')
+
